9aae5df36ae315c374d1316d38853eb12daf115a
dhuseljic <dhuseljic@uni-kassel.de>
2022-10-11 08:35:24 +0200

    updated readme with results, update eval notebook

--- notebooks/eval.py
+++ notebooks/eval.py
@@ -2,10 +2,10 @@
 import json
 
 import numpy as np
-import pylab as plt
 import pandas as pd
 
 from pathlib import Path
+from omegaconf import OmegaConf
 
 result_path = Path('../results/')
 
@@ -13,41 +13,77 @@ result_path = Path('../results/')
 # %%
 
 
-def get_results(result_path, glob_pattern):
-    results = []
+def get_experiments(result_path, glob_pattern):
+    experiments = []
     for exp_path in result_path.glob(glob_pattern):
+
         exp_json = exp_path / 'results_final.json'
+        exp_cfg = exp_path / '.hydra' / 'config.yaml'
+
         with open(exp_json, 'r') as f:
             data = json.load(f)
+            cfg = {}  # OmegaConf.load(exp_cfg)
 
         result_dict_final_epoch = data['test_history'][-1]
-        results.append(result_dict_final_epoch)
-    return results
+        experiments.append({'results': result_dict_final_epoch, 'cfg': cfg})
+    return experiments
 
 
-results = get_results(result_path, glob_pattern='CIFAR10__resnet18_sngp__[1-9]')
-results
-
+experiments = get_experiments(result_path / 'CIFAR10__resnet18', glob_pattern='seed*')
+print(f'Found {len(experiments)} experiments')
 
 # %%
 
-def get_metric_dict(results):
-    metrics = list(results[0].keys())
-    # accuracies = np.array([d['test_acc1']for d in results])
+
+def get_metric_dict(experiments, ignore_metrics=[]):
+    metric_names = list(experiments[0]['results'].keys())
     d = {}
-    for metric in metrics:
-        value = np.mean([d[metric] for d in results])
-        d[metric] = value
+    for metric_name in metric_names:
+        if metric_name in ignore_metrics:
+            continue
+        value = np.mean([exp['results'][metric_name] for exp in experiments])
+        d[metric_name] = value
     return d
 
-exp_names = {
-    "default": "CIFAR10__resnet18_sngp__[1-9]",
-    "num_inducing4096": "CIFAR10__resnet18_sngp__num_inducing4096__[1-9]",
-}
-data = [get_metric_dict(get_results(result_path, glob_pattern=n)) for _, n in exp_names.items()]
-df = pd.DataFrame(data, index=exp_names.keys())
-print(df.to_markdown(floatfmt=".3f"))
 
+# %%
+"""Results for default parameters."""
+
+exp_names = {
+    "resnet18": "CIFAR10__resnet18",
+    "resnet18_sngp": "CIFAR10__resnet18_sngp",
+}
+ignore_metrics = ['test_SVHN_conf_auroc', 'test_SVHN_conf_aupr']
+
+data = []
+for key, name in exp_names.items():
+    experiments = get_experiments(result_path / name, glob_pattern='seed*')
+    metric_dict = get_metric_dict(experiments, ignore_metrics=ignore_metrics)
+    data.append(metric_dict)
+
+df = pd.DataFrame(data, index=exp_names.keys())
+df.to_markdown()
+
+
+# %%
+"""SNGP ablation."""
+
+exp_names = {
+    "default": "CIFAR10__resnet18_sngp",
+    "kernel_scale=10": "CIFAR10__resnet18_sngp__kernel_scale10",
+    "num_inducing=4096": "CIFAR10__resnet18_sngp__num_inducing4096",
+    "scale_features=False": "CIFAR10__resnet18_sngp__scale_features",
+}
+ignore_metrics = ['test_SVHN_conf_auroc', 'test_SVHN_conf_aupr']
+
+data = []
+for key, name in exp_names.items():
+    experiments = get_experiments(result_path / name, glob_pattern='seed*')
+    metric_dict = get_metric_dict(experiments, ignore_metrics=ignore_metrics)
+    data.append(metric_dict)
+
+df = pd.DataFrame(data, index=exp_names.keys())
+df.to_markdown()
 
 
 # %%
