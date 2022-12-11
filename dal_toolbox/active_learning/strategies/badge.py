46dd8b18c8c3d505ece1d01fd752b68211a2b53f
dhuseljic <dhuseljic@uni-kassel.de>
2022-12-11 15:25:44 +0100

    clean up code

--- dal_toolbox/active_learning/strategies/badge.py
+++ dal_toolbox/active_learning/strategies/badge.py
@@ -10,59 +10,6 @@ from sklearn.utils.extmath import stable_cumsum, row_norms
 from .query import Query
 
 
-# kmeans ++ initialization
-def init_centers(X, K):
-    # TODO: in k-means++ we sample uniformly the center
-
-    # # kmeans++ from algo in paper
-    # np.random.seed(0)
-    # idx = np.random.choice(range(len(X)))
-    # indices = [idx]
-    # centers = [X[idx]]
-    # dist_mat = []
-    # for i in range(K-1):
-    #     dist = np.sqrt(np.sum((X - centers[-1])**2, axis=-1))
-    #     dist_mat.append(dist)
-    #     dist_mat_min= np.min(dist_mat, axis=0)
-    #     dist_mat_squared = dist_mat_min**2
-    #     if np.all(dist_mat_squared == 0):
-    #         raise ValueError('All distances to the centers are zero!')
-    #     p = dist_mat_squared / np.sum(dist_mat_squared)
-    #     idx = np.random.choice(range(len(X)), p=p.squeeze())
-    #     indices.append(idx)
-    #     centers.append(X[idx])
-
-    # ind = np.argmax([np.linalg.norm(s, 2) for s in X])
-    grad_norm = np.linalg.norm(X, ord=2, axis=1)
-    ind = np.argmax(grad_norm)
-    mu = [X[ind]]
-    indsAll = [ind]
-    centInds = [0.] * len(X)
-    cent = 0
-    print('#Samps\tTotal Distance')
-    while len(mu) < K:
-        if len(mu) == 1:
-            D2 = pairwise_distances(X, mu).ravel().astype(float)
-        else:
-            newD = pairwise_distances(X, [mu[-1]]).ravel().astype(float)
-            for i in range(len(X)):
-                if D2[i] > newD[i]:
-                    centInds[i] = cent
-                    D2[i] = newD[i]
-        print(str(len(mu)) + '\t' + str(np.sum(D2)), flush=True)
-        # if sum(D2) == 0.0:
-        #     pdb.set_trace()
-        D2 = D2.ravel().astype(float)
-        Ddist = (D2 ** 2) / np.sum(D2 ** 2)
-        customDist = stats.rv_discrete(name='custm', values=(np.arange(len(D2)), Ddist))
-        ind = customDist.rvs(size=1)[0]
-        while ind in indsAll:
-            raise ValueError('All distances to the centers are zero!')
-            ind = customDist.rvs(size=1)[0]
-        mu.append(X[ind])
-        indsAll.append(ind)
-        cent += 1
-    return indsAll
 
 
 class Badge(Query):
@@ -82,45 +29,24 @@ class Badge(Query):
 
         unlabeled_dataloader = DataLoader(dataset, batch_size=self.batch_size, sampler=unlabeled_indices)
         grad_embedding = model.get_grad_embedding(unlabeled_dataloader, device=self.device)
-        # chosen = init_centers(grad_embedding.numpy(), acq_size)
-        chosen = kmeans_plusplus(grad_embedding.numpy(), acq_size, rng=self.np_rng)
+        chosen = kmeans_plusplus(grad_embedding.numpy(), acq_size, np_rng=self.np_rng)
         return [unlabeled_indices[idx] for idx in chosen]
 
 
-def kmeans_plusplus(X, n_clusters, rng):
+def kmeans_plusplus(X, n_clusters, np_rng):
     _, indices = _kmeans_plusplus(
         X,
         n_clusters=n_clusters,
         x_squared_norms=row_norms(X, squared=True),
-        random_state=rng,
+        random_state=np_rng,
     )
     indices = np.unique(indices).tolist()
     return indices
 
 
 def _kmeans_plusplus(X, n_clusters, x_squared_norms, random_state):
-    """Computational component for initialization of n_clusters by k-means++ base on sklearn.
-
-    Parameters
-    ----------
-    X : {ndarray, sparse matrix} of shape (n_samples, n_features)
-        The data to pick seeds for.
-    n_clusters : int
-        The number of seeds to choose.
-    x_squared_norms : ndarray of shape (n_samples,)
-        Squared Euclidean norm of each data point.
-    random_state : RandomState instance
-        The generator used to initialize the centers.
-        See :term:`Glossary <random_state>`.
-
-    Returns
-    -------
-    centers : ndarray of shape (n_clusters, n_features)
-        The initial centers for k-means.
-    indices : ndarray of shape (n_clusters,)
-        The index location of the chosen centers in the data array X. For a
-        given index and center, X[index] = center.
-    """
+    # kmeans++ from sklearn see:
+    # https://github.com/scikit-learn/scikit-learn/blob/dc580a8ef5ee2a8aea80498388690e2213118efd/sklearn/cluster/_kmeans.py#L164
     n_samples, n_features = X.shape
     centers = np.empty((n_clusters, n_features), dtype=X.dtype)
 
@@ -165,3 +91,58 @@ def _kmeans_plusplus(X, n_clusters, x_squared_norms, random_state):
         indices[c] = best_candidate
 
     return centers, indices
+
+
+# kmeans ++ initialization
+def init_centers(X, K):
+    # Code from: https://github.com/JordanAsh/badge/blob/master/query_strategies/badge_sampling.py
+    # # kmeans++ from algo in paper
+    # np.random.seed(0)
+    # idx = np.random.choice(range(len(X)))
+    # indices = [idx]
+    # centers = [X[idx]]
+    # dist_mat = []
+    # for i in range(K-1):
+    #     dist = np.sqrt(np.sum((X - centers[-1])**2, axis=-1))
+    #     dist_mat.append(dist)
+    #     dist_mat_min= np.min(dist_mat, axis=0)
+    #     dist_mat_squared = dist_mat_min**2
+    #     if np.all(dist_mat_squared == 0):
+    #         raise ValueError('All distances to the centers are zero!')
+    #     p = dist_mat_squared / np.sum(dist_mat_squared)
+    #     idx = np.random.choice(range(len(X)), p=p.squeeze())
+    #     indices.append(idx)
+    #     centers.append(X[idx])
+
+
+    # grad_norm = np.linalg.norm(X, ord=2, axis=1)
+    # ind = np.argmax(grad_norm)
+    ind = np.argmax([np.linalg.norm(s, 2) for s in X])
+    mu = [X[ind]]
+    indsAll = [ind]
+    centInds = [0.] * len(X)
+    cent = 0
+    print('#Samps\tTotal Distance')
+    while len(mu) < K:
+        if len(mu) == 1:
+            D2 = pairwise_distances(X, mu).ravel().astype(float)
+        else:
+            newD = pairwise_distances(X, [mu[-1]]).ravel().astype(float)
+            for i in range(len(X)):
+                if D2[i] > newD[i]:
+                    centInds[i] = cent
+                    D2[i] = newD[i]
+        print(str(len(mu)) + '\t' + str(np.sum(D2)), flush=True)
+        # if sum(D2) == 0.0:
+        #     pdb.set_trace()
+        D2 = D2.ravel().astype(float)
+        Ddist = (D2 ** 2) / np.sum(D2 ** 2)
+        customDist = stats.rv_discrete(name='custm', values=(np.arange(len(D2)), Ddist))
+        ind = customDist.rvs(size=1)[0]
+        while ind in indsAll:
+            raise ValueError('All distances to the centers are zero!')
+            ind = customDist.rvs(size=1)[0]
+        mu.append(X[ind])
+        indsAll.append(ind)
+        cent += 1
+    return indsAll
