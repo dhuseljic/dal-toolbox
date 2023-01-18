1b5fa2573b556fda3f7bd50e00ad3c15de2c4894
dhuseljic <dhuseljic@uni-kassel.de>
2023-01-18 14:29:36 +0100

    fix for pseudo labels

--- experiments/semi_supervised_learning/pseudo_labels.py
+++ experiments/semi_supervised_learning/pseudo_labels.py
@@ -9,11 +9,11 @@ from torch.utils.data import DataLoader
 from omegaconf import OmegaConf
 
 from dal_toolbox.datasets import build_ssl_dataset
-from dal_toolbox.models import build_model
+from dal_toolbox.models import build_ssl_model
 from dal_toolbox.utils import seed_everything
 
 
-@hydra.main(version_base=None, config_path="./configs", config_name="semi_supervised_learning")
+@hydra.main(version_base=None, config_path="./configs", config_name="pseudo_labels")
 def main(args):
     # Initial Setup (Seed, create output folder, SummaryWriter and results-container init)
     logging.info('Using config: \n%s', OmegaConf.to_yaml(args))
@@ -28,27 +28,20 @@ def main(args):
     supervised_loader = DataLoader(lb_ds, batch_size=args.model.batch_size, shuffle=True)
     unsupervised_loader = DataLoader(ulb_ds, batch_size=int(args.model.batch_size*args.u_ratio), shuffle=True)
     val_loader = DataLoader(val_ds, batch_size=args.val_batch_size)
-    dataloaders = {
-        "train_sup": supervised_loader,
-        "train_unsup": unsupervised_loader
-    }
+    dataloaders = {"train_sup": supervised_loader, "train_unsup": unsupervised_loader}
 
     # Setup Model
     logging.info('Building model: %s', args.model.name)
-    model_dict = build_model(args, n_classes=ds_info['n_classes'])
+    model_dict = build_ssl_model(args, n_classes=ds_info['n_classes'])
     model, train_one_epoch, evaluate = model_dict['model'], model_dict['train_one_epoch'], model_dict['evaluate']
     lr_scheduler = model_dict['lr_scheduler']
 
     # Training Process
     history_train, history_test = [], []
     for i_epoch in range(args.model.n_epochs):
-        # Train model for one epoch
         logging.info('Training epoch %s', i_epoch)
-        train_stats = train_one_epoch(
-            model, dataloaders, **model_dict['train_kwargs'], epoch=i_epoch
-        )
-        if lr_scheduler:
-            lr_scheduler.step()
+        train_stats = train_one_epoch(model, dataloaders, **model_dict['train_kwargs'], epoch=i_epoch)
+        lr_scheduler.step()
         for key, value in train_stats.items():
             writer.add_scalar(tag=f"train/{key}", scalar_value=value, global_step=i_epoch)
         logging.info('Training stats: %s', train_stats)
