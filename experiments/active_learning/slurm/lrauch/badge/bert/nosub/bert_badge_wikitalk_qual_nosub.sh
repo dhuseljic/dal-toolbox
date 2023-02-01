1afb03cb60a5395174e8556b34fdd649a6ab6461
Lukas Rauch <lukas.rauch@uni-kassel.de>
2023-02-01 11:28:33 +0100

    add slurm scripts for nosub for 5 different datasets

--- experiments/active_learning/slurm/lrauch/badge/bert/nosub/bert_badge_wikitalk_qual_nosub.sh
+++ experiments/active_learning/slurm/lrauch/badge/bert/nosub/bert_badge_wikitalk_qual_nosub.sh
@@ -4,7 +4,7 @@
 #SBATCH --cpus-per-task=8
 #SBATCH --gres=gpu:1
 #SBATCH --partition=main
-#SBATCH --job-name=glae_agnews_badge_bert_nosub
+#SBATCH --job-name=glae_wikitalk_badge_bert_nosub
 #SBATCH --output=/mnt/work/lrauch/logs/active_learning/%x_%a.log
 #SBATCH --array=1-5%5
 date;hostname;pwd
@@ -16,27 +16,28 @@ export CUDA_LAUNCH_BLOCKING=1
 export HYDRA_FULL_ERROR=1
 
 MODEL=bert
-DATASET=agnews
+DATASET=wikitalk
 STRATEGY=badge
 
 N_INIT=100
 ACQ_SIZE=100
 N_ACQ=15
-
-GROUP=bert_badge_agnews_nosub
+GROUP=bert_badge_wikitalk_nosub
 TRAIN_SUBSET=0
 
-OUTPUT_DIR=/mnt/work/lrauch/glae-results/${DATASET}/$MODEL/${STRATEGY}/N_INIT${N_INIT}__ACQ_SIZE${ACQ_SIZE}__N_ACQ${N_ACQ}/seed${SLURM_ARRAY_TASK_ID}
+OUTPUT_DIR=/mnt/work/lrauch/glae-results/${DATASET}/$MODEL/${STRATEGY}/nosub/N_INIT${N_INIT}__ACQ_SIZE${ACQ_SIZE}__N_ACQ${N_ACQ}/seed${SLURM_ARRAY_TASK_ID}
 echo "Writing results to ${OUTPUT_DIR}"
 
 srun python -u al_txt.py \
     model=$MODEL \
     dataset=$DATASET \
     output_dir=$OUTPUT_DIR \
+    al_strategy=$STRATEGY \
     random_seed=$SLURM_ARRAY_TASK_ID \
     al_strategy=$STRATEGY \
     al_cycle.n_init=$N_INIT \
     al_cycle.acq_size=$ACQ_SIZE \
     al_cycle.n_acq=$N_ACQ \
-    wandb.group=$GROUP \
+    wandb.group=$GROUP \ 
     model.dataset.train_subset=TRAIN_SUBSET
+
