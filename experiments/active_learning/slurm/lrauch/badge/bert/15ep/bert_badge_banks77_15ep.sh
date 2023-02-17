058fb344d8a8b724529af1824d3a22ace984983a
Lukas Rauch <lukas.rauch@uni-kassel.de>
2023-02-17 10:42:09 +0100

    Add bert with 15 epochs training scripts for slurm

--- experiments/active_learning/slurm/lrauch/badge/bert/15ep/bert_badge_banks77_15ep.sh
+++ experiments/active_learning/slurm/lrauch/badge/bert/15ep/bert_badge_banks77_15ep.sh
@@ -4,7 +4,7 @@
 #SBATCH --cpus-per-task=8
 #SBATCH --gres=gpu:1
 #SBATCH --partition=main
-#SBATCH --job-name=glae_agnews_badge_bert_30ep
+#SBATCH --job-name=glae_banks77_badge_bert_15ep
 #SBATCH --output=/mnt/work/lrauch/logs/active_learning/%x_%a.log
 #SBATCH --array=1-5%5
 date;hostname;pwd
@@ -16,16 +16,16 @@ export CUDA_LAUNCH_BLOCKING=1
 export HYDRA_FULL_ERROR=1
 
 MODEL=bert
-DATASET=agnews
+DATASET=banks77
 STRATEGY=badge
 
 N_INIT=100
 ACQ_SIZE=100
 N_ACQ=15
-GROUP=bert_badge_agnews_nosub
-TRAIN_SUBSET=0
+GROUP=bert_badge_banks77_15ep
+N_EPOCHS=15
 
-OUTPUT_DIR=/mnt/work/glae/glae-results/${DATASET}/$MODEL/${STRATEGY}/nosub/N_INIT${N_INIT}__ACQ_SIZE${ACQ_SIZE}__N_ACQ${N_ACQ}/seed${SLURM_ARRAY_TASK_ID}
+OUTPUT_DIR=/mnt/work/glae/glae-results/${DATASET}/$MODEL/${STRATEGY}/15ep/N_INIT${N_INIT}__ACQ_SIZE${ACQ_SIZE}__N_ACQ${N_ACQ}/seed${SLURM_ARRAY_TASK_ID}
 echo "Writing results to ${OUTPUT_DIR}"
 
 srun python -u al_txt.py \
@@ -38,4 +38,4 @@ srun python -u al_txt.py \
     al_cycle.acq_size=$ACQ_SIZE \
     al_cycle.n_acq=$N_ACQ \
     wandb.group=$GROUP \
-    dataset.train_subset=$TRAIN_SUBSET
+    model.n_epochs=$N_EPOCHS
