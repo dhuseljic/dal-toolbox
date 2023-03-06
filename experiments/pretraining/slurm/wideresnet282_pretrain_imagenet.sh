9a00c287bbf29b68b97e85b216de826ff0a5aa16
phahn <hahn-paul@t-online.de>
2023-03-06 10:32:06 +0100

    Updated Slurm using scratch for wrs282

--- experiments/pretraining/slurm/wideresnet282_pretrain_imagenet.sh
+++ experiments/pretraining/slurm/wideresnet282_pretrain_imagenet.sh
@@ -8,6 +8,8 @@
 #SBATCH --output=/mnt/stud/work/phahn/uncertainty/logs/%x_%j.log
 source /mnt/stud/home/phahn/.zshrc
 
+rsync -avz /mnt/datasets/imagenet/ILSVRC2012/downloads /scratch/phahn/
+
 git checkout 38-pretraining-time-too-long
 
 conda activate uncertainty_evaluation
@@ -19,7 +21,7 @@ echo "Saving results to $OUTPUT_DIR"
 
 srun python -u experiments/pretraining/main.py \
     dataset=Imagenet \
-    dataset_path=/mnt/datasets/imagenet/ILSVRC2012/downloads/ \
+    dataset_path=/scratch/phahn/downloads/ \
     model=wideresnet282 \
     output_dir=$OUTPUT_DIR \
     random_seed=${SLURM_JOB_ID}
