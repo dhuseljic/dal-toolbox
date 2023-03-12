2dd1579d04395ae80fd2be1fa54d418c71eb47f5
phahn <hahn-paul@t-online.de>
2023-03-12 11:31:34 +0100

    Added Label smoothing to Uncertainty and created slurm

--- experiments/uncertainty/slurm/phahn/resnet18_mixup.sh
+++ experiments/uncertainty/slurm/phahn/resnet18_mixup.sh
@@ -4,12 +4,14 @@
 #SBATCH --cpus-per-task=8
 #SBATCH --gres=gpu:1
 #SBATCH --partition=main
-#SBATCH --job-name=WRS2810-DUE
+#SBATCH --job-name=RES-MIX
 #SBATCH --output=/mnt/stud/work/phahn/uncertainty/logs/%x_%A_%a.log
-#SBATCH --array=1-8%8
+#SBATCH --array=1-3%3
 source /mnt/stud/home/phahn/.zshrc
 
-conda activate torchal
+git checkout 40-implement-mixup-for-uncertainty-calibration
+
+conda activate uncertainty_evaluation
 
 cd /mnt/stud/work/phahn/uncertainty/uncertainty-evaluation
 
@@ -19,6 +21,7 @@ echo "Saving results to $OUTPUT_DIR"
 srun python -u uncertainty.py \
     dataset=CIFAR10 \
     ood_datasets=\[SVHN\] \
-    model=wideresnet2810_due \
+    model=resnet18_mixup \
+    model.alpha=0.5 \
     output_dir=$OUTPUT_DIR \
     random_seed=${SLURM_ARRAY_TASK_ID}
