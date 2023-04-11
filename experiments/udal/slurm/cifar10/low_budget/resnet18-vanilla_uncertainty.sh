8255d14b9306289a15102c945c531b05a5aa9f92
Denis Huseljic <dhuseljic@compute.ies.uni-kassel.de>
2023-04-11 10:59:17 +0200

    added cifar lowbudget slurm scripts

--- experiments/udal/slurm/cifar10/low_budget/resnet18-vanilla_uncertainty.sh
+++ experiments/udal/slurm/cifar10/low_budget/resnet18-vanilla_uncertainty.sh
@@ -5,8 +5,8 @@
 #SBATCH --gres=gpu:1
 #SBATCH --partition=main
 #SBATCH --array=1-3%10
-#SBATCH --job-name=al_coreset_resnet18-labelsmoothing_cifar10
-#SBATCH --output=/mnt/work/dhuseljic/logs/active_learning/%A_%a_%x.log
+#SBATCH --job-name=al_uncertainty_resnet18_cifar10
+#SBATCH --output=/mnt/work/dhuseljic/logs/udal/active_learning/%A_%a_%x.log
 date;hostname;pwd
 source /mnt/home/dhuseljic/.zshrc
 conda activate uncertainty_evaluation
@@ -15,24 +15,23 @@ cd /mnt/home/dhuseljic/projects/dal-toolbox/experiments/udal/
 export CUDA_LAUNCH_BLOCKING=1
 export HYDRA_FULL_ERROR=1
 
-model=resnet18_labelsmoothing
+model=resnet18
 dataset=CIFAR10
-dataset_path=/tmp/
 
-al_strat=coreset
+al_strat=uncertainty
 n_init=100
 acq_size=100
 n_acq=19
 random_seed=$SLURM_ARRAY_TASK_ID
 init_pool_file=/mnt/home/dhuseljic/projects/dal-toolbox/experiments/udal/initial_pools/CIFAR10/random_${n_init}_seed${random_seed}.json
 
-output_dir=/mnt/work/deep_al/results/active_learning/${dataset}/${model}/${al_strat}/N_INIT${n_init}__ACQ_SIZE${acq_size}__N_ACQ${n_acq}/seed${random_seed}/
+output_dir=/mnt/work/deep_al/results/udal/active_learning/${dataset}/${model}/${al_strat}/N_INIT${n_init}__ACQ_SIZE${acq_size}__N_ACQ${n_acq}/seed${random_seed}/
 
 echo "Starting script. Writing results to ${output_dir}"
 srun python -u active_learning.py \
 	model=$model \
-	model.optimizer.lr=1e-2 \
-	model.optimizer.weight_decay=5e-2 \
+	model.optimizer.lr=0.0766450142252356 \
+	model.optimizer.weight_decay=0.014745096744053244 \
 	dataset=$dataset \
 	dataset_path=/mnt/work/dhuseljic/datasets \
 	al_strategy=$al_strat \
