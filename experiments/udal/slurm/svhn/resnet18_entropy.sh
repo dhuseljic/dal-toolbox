672ea5c105523aa8ed2efe25d78dbe8fdfc853ae
dhuseljic <dhuseljic@uni-kassel.de>
2023-04-25 14:19:55 +0200

    update slurm scripts

--- experiments/udal/slurm/svhn/resnet18_entropy.sh
+++ experiments/udal/slurm/svhn/resnet18_entropy.sh
@@ -6,32 +6,27 @@
 #SBATCH --partition=main
 #SBATCH --array=1-3%10
 #SBATCH --job-name=al_random_resnet18_svhn
-#SBATCH --output=/mnt/work/dhuseljic/logs/active_learning/%A_%a_%x.log
+#SBATCH --output=/mnt/work/dhuseljic/logs/udal/active_learning/%A_%a_%x.log
 date;hostname;pwd
-source /mnt/home/dhuseljic/.zshrc
-conda activate uncertainty_evaluation
+source activate uncertainty_evaluation
 
 cd /mnt/home/dhuseljic/projects/dal-toolbox/experiments/udal/
-export CUDA_LAUNCH_BLOCKING=1
-export HYDRA_FULL_ERROR=1
 
 model=resnet18
 dataset=SVHN
-
-al_strat=random
+al_strat=uncertainty
 n_init=100
 acq_size=100
 n_acq=19
 random_seed=$SLURM_ARRAY_TASK_ID
 init_pool_file=/mnt/home/dhuseljic/projects/dal-toolbox/experiments/udal/initial_pools/SVHN/random_${n_init}_seed${random_seed}.json
-
-output_dir=/mnt/work/deep_al/results/active_learning/${dataset}/${model}/${al_strat}/N_INIT${n_init}__ACQ_SIZE${acq_size}__N_ACQ${n_acq}/seed${random_seed}/
+output_dir=/mnt/work/deep_al/results/udal/active_learning/${dataset}/${model}/${al_strat}/N_INIT${n_init}__ACQ_SIZE${acq_size}__N_ACQ${n_acq}/seed${random_seed}/
 
 echo "Starting script. Writing results to ${output_dir}"
 srun python -u active_learning.py \
 	model=$model \
-	model.optimizer.lr=1e-2 \
-	model.optimizer.weight_decay=5e-2 \
+	model.optimizer.lr=0.01 \
+	model.optimizer.weight_decay=0.005 \
 	dataset=$dataset \
 	dataset_path=/mnt/work/dhuseljic/datasets \
 	al_strategy=$al_strat \
