bd80fb67ee250c41edc1d657e2c65cd5aaf59eac
dhuseljic <dhuseljic@uni-kassel.de>
2023-04-29 17:03:02 +0200

    update hparams for ensemble

--- experiments/udal/slurm/svhn/epistemic/resnet18-ensemble_random.sh
+++ experiments/udal/slurm/svhn/epistemic/resnet18-ensemble_random.sh
@@ -26,8 +26,8 @@ echo "Starting script. Writing results to ${output_dir}"
 srun python -u active_learning.py \
 	model=$model \
 	model.batch_size=32 \
-	model.optimizer.lr=0.01 \
-	model.optimizer.weight_decay=0.005 \
+	model.optimizer.lr=0.06 \
+	model.optimizer.weight_decay=0.001 \
 	dataset=$dataset \
 	ood_datasets=\[CIFAR10,\ CIFAR100\] \
 	dataset_path=/mnt/work/dhuseljic/datasets \
