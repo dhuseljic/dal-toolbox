de70980a071a728e27c2a52c5db1917bd7f6a65b
dhuseljic <dhuseljic@uni-kassel.de>
2023-05-22 14:36:27 +0200

    updated slurm scripts

--- experiments/hyperparameters/slurm/graphical_abstract_random.sh
+++ experiments/hyperparameters/slurm/graphical_abstract_random.sh
@@ -6,10 +6,9 @@
 #SBATCH --ntasks=1
 #SBATCH --mem=32GB
 #SBATCH --array=0-26
-# Array currently configured for random_seed 1 only
 date;hostname;pwd
 source activate dal-toolbox
-cd /mnt/home/dhuseljic/projects/dal-toolbox/experiments/hyperparameters/
+cd ~/projects/dal-toolbox/experiments/hyperparameters/
 
 # Define the range of hyperparameters 
 learning_rates=(0.001 0.01 0.1)
@@ -25,7 +24,7 @@ weight_decay=${weight_decays[$index / 3 % 3]}
 random_seed=${random_seeds[$index / 9]}
 
 al_strategy=random
-output_dir=/mnt/work/dhuseljic/results/hyperparameters/graphical_abstract/${al_strategy}/lr${learning_rate}_wd${weight_decay}/seed${random_seed}
+output_dir=/mnt/work/deep_al/results/hyperparameters/graphical_abstract/${al_strategy}/lr${learning_rate}_wd${weight_decay}/seed${random_seed}
 
 # Run the deep learning script with the current hyperparameters
 python -u active_learning.py \
