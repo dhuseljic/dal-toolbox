c37ea91fd3600a684412ab4443b5c6456dec4623
Paul Hahn <phahn@cluster.ies.uni-kassel.de>
2023-05-23 11:35:14 +0200

    Fixed bugged slurm script

--- experiments/hyperparameters/slurm/hparam.sh
+++ experiments/hyperparameters/slurm/hparam.sh
@@ -8,13 +8,12 @@
 #SBATCH --mem=32GB
 #SBATCH --array=0-26%4
 date;hostname;pwd
-source /mnt/stud/home/phahn/.zshrc
-cd /mnt/stud/work/phahn/uncertainty/uncertainty-evaluation/experiments/hyperparameters/
+source ~/.bashrc
+source activate dal-toolbox
+cd /mnt/stud/work/phahn/uncertainty/dal-toolbox/experiments/hyperparameters/
 
-rm /mnt/stud/work/phahn/uncertainty/uncertainty-evaluation/.git/index.lock
-git checkout hyperparameters
-
-conda activate uncertainty_evaluation
+# rm /mnt/stud/work/phahn/uncertainty/dal-toolbox/.git/index.lock
+# git checkout hyperparameters
 
 # Define the range of hyperparameters 
 learning_rates=(0.001 0.01 0.1)
@@ -31,12 +30,12 @@ random_seed=${random_seeds[$index / 9]}
 
 al_strategy=random
 queried_indices_json=/mnt/work/deep_al/results/hyperparameters/graphical_abstract/${al_strategy}/lr${learning_rate}_wd${weight_decay}/seed${random_seed}/queried_indices.json
-ouput_dir=/mnt/stud/work/phahn/uncertainty/output/hyperparameters/${al_strategy}/lr${learning_rate}_wd${weight_decay}/seed${random_seed}/
+output_dir=/mnt/stud/work/phahn/uncertainty/output/hyperparameters/${al_strategy}/lr${learning_rate}_wd${weight_decay}/seed${random_seed}/
 
 python -u hparam.py \
     queried_indices_json=$queried_indices_json \
     output_dir=$output_dir \
     random_seed=$random_seed \
-    dataset_path=/mnt/stud/work/phahn/uncertainty/uncertainty-evaluation/data \
+    dataset_path=/mnt/stud/work/phahn/uncertainty/dal-toolbox/data \
     num_cpus=4 \
-    num_gpus=0.5 \
+    num_gpus=0.5
