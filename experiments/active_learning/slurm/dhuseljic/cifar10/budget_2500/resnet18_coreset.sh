57c8a8b4e98e8ef0536e410c7d94866c017ab8fa
dhuseljic <dhuseljic@uni-kassel.de>
2023-05-25 18:56:53 +0200

    updates slurm scripts for al baselines

---
+++ experiments/active_learning/slurm/dhuseljic/cifar10/budget_2500/resnet18_coreset.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/zsh
+#SBATCH --mem=32gb
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --gres=gpu:1
+#SBATCH --partition=main
+#SBATCH --array=1-3%3
+#SBATCH --job-name=al_baselines
+#SBATCH --output=/mnt/work/dhuseljic/logs/al_baselines/%A_%a__%x.log
+date;hostname;pwd
+source activate dal-toolbox
+cd ~/projects/dal-toolbox/experiments/active_learning/
+
+model=resnet18
+dataset=CIFAR10
+
+al_strat=coreset
+n_init=250
+acq_size=250
+n_acq=9
+budget=$((n_init + n_acq * acq_size))
+random_seed=$SLURM_ARRAY_TASK_ID
+output_dir=/mnt/work/deep_al/results/al_baselines/${dataset}/${model}/${al_strat}/budget_${budget}/seed${random_seed}/
+
+srun python -u active_learning.py \
+	model=$model \
+	model.optimizer.lr=1e-3 \
+	model.optimizer.weight_decay=5e-2 \
+	dataset=$dataset \
+	dataset_path=/mnt/work/dhuseljic/datasets \
+	al_strategy=$al_strat \
+	al_cycle.n_init=$n_init \
+	al_cycle.acq_size=$acq_size \
+	al_cycle.n_acq=$n_acq \
+	random_seed=$random_seed \
+	output_dir=$output_dir
