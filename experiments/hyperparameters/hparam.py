83871ba59fba63752e4ff3bc9a15c71acd0ee38c
dhuseljic <dhuseljic@uni-kassel.de>
2023-05-27 11:57:04 +0200

    updated hparam so that the final acc get computed on whole dataset

--- experiments/hyperparameters/hparam.py
+++ experiments/hyperparameters/hparam.py
@@ -19,7 +19,7 @@ from dal_toolbox.utils import seed_everything
 from dal_toolbox.models.utils.callbacks import MetricLogger
 
 
-def train(config, args, al_dataset, test_ds):
+def train(config, args, al_dataset):
     seed_everything(args.random_seed+config['__trial_index__']+100)
 
     # Train test split
@@ -30,11 +30,28 @@ def train(config, args, al_dataset, test_ds):
     # Create dataloaders
     train_loader = DataLoader(train_ds, batch_size=args.train_batch_size, shuffle=True, drop_last=True)
     val_loader = DataLoader(val_ds, batch_size=args.predict_batch_size, shuffle=False)
-    test_loader = DataLoader(test_ds, batch_size=args.predict_batch_size, shuffle=False)
 
     # Create model
+    model = build_model(args, lr=config['lr'], weight_decay=config['weight_decay'])
+
+    # Train model
+    trainer = L.Trainer(
+        enable_checkpointing=False,
+        max_epochs=args.num_epochs,
+        callbacks=[MetricLogger(use_print=True)],
+        enable_progress_bar=False,
+        fast_dev_run=10,
+    )
+    trainer.fit(model, train_loader, val_dataloaders=val_loader)
+    logged_metrics = trainer.logged_metrics
+    val_stats = {name: metric.item() for name, metric in logged_metrics.items() if isinstance(metric, torch.Tensor)}
+
+    return val_stats
+
+
+def build_model(args, lr, weight_decay):
     model = models.deterministic.resnet.ResNet18(10)
-    optimizer = torch.optim.SGD(model.parameters(), lr=config['lr'], weight_decay=config['weight_decay'], momentum=.9)
+    optimizer = torch.optim.SGD(model.parameters(), lr=lr, weight_decay=weight_decay, momentum=.9)
     lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=args.num_epochs)
     model = models.deterministic.DeterministicModel(
         model,
@@ -43,33 +60,7 @@ def train(config, args, al_dataset, test_ds):
         train_metrics={'train_acc': metrics.Accuracy()},
         val_metrics={'val_acc': metrics.Accuracy(), 'val_nll': metrics.CrossEntropy()}
     )
-
-    # Train model
-    trainer = L.Trainer(
-        enable_checkpointing=False,
-        max_epochs=args.num_epochs,
-        callbacks=[MetricLogger(use_print=True)],
-        enable_progress_bar=False
-    )
-    trainer.fit(model, train_loader, val_dataloaders=val_loader)
-
-    logged_metrics = trainer.logged_metrics
-
-    # Evaluation
-    predictions_id = trainer.predict(model, test_loader)
-    logits_id = torch.cat([pred[0] for pred in predictions_id])
-    targets_id = torch.cat([pred[1] for pred in predictions_id])
-
-    test_stats = {
-        'test_acc': metrics.Accuracy()(logits_id, targets_id).item(),
-        'test_nll': metrics.CrossEntropy()(logits_id, targets_id).item()
-    }
-
-    res = {name: metric.item() for name, metric in logged_metrics.items()
-           if isinstance(metric, torch.Tensor)}
-    res.update(test_stats)
-
-    return res
+    return model
 
 
 @hydra.main(version_base=None, config_path="./configs", config_name="hparam")
@@ -97,13 +88,7 @@ def main(args):
     search_space = {"lr": tune.uniform(1e-4, .5), "weight_decay": tune.uniform(0, .1)}
 
     objective = tune.with_resources(train, resources={'cpu': args.num_cpus, 'gpu': args.num_gpus})
-    objective = tune.with_parameters(
-        objective,
-        args=args,
-        al_dataset=al_dataset,
-        test_ds=data.test_dataset,
-    )
-
+    objective = tune.with_parameters(objective, args=args, al_dataset=al_dataset)
     search_alg = OptunaSearch(points_to_evaluate=[{'lr': args.lr, 'weight_decay': args.weight_decay}])
     search_alg = Repeater(search_alg, repeat=args.num_reps)
     tune_config = tune.TuneConfig(search_alg=search_alg, num_samples=args.num_opt_samples *
@@ -114,19 +99,39 @@ def main(args):
     print('Best Hyperparameters for NLL: {}'.format(results.get_best_result(metric='val_nll', mode='min').config))
     print('Best Hyperparameters for ACC: {}'.format(results.get_best_result(metric='val_acc', mode='max').config))
 
+    # Fit model with best hyperparameters
+    best_config = results.get_best_result(metric='val_acc', mode='max').config
+    print(f'Training final model using the best possible parameters {best_config}')
+    train_loader_all = DataLoader(al_dataset, batch_size=args.train_batch_size, shuffle=True, drop_last=True)
+    model = build_model(args, lr=best_config['lr'], weight_decay=best_config['weight_decay'])
+    trainer = L.Trainer(
+        enable_checkpointing=False,
+        max_epochs=args.num_epochs,
+        callbacks=[MetricLogger(use_print=True)],
+        enable_progress_bar=False,
+    )
+    trainer.fit(model, train_loader_all)
+
+    test_loader = DataLoader(data.test_dataset, batch_size=args.predict_batch_size, shuffle=False)
+    predictions_id = trainer.predict(model, test_loader)
+    logits_id = torch.cat([pred[0] for pred in predictions_id])
+    targets_id = torch.cat([pred[1] for pred in predictions_id])
+    test_stats = {
+        'test_acc': metrics.Accuracy()(logits_id, targets_id).item(),
+        'test_nll': metrics.CrossEntropy()(logits_id, targets_id).item()
+    }
+    print(f'Final results: {test_stats}')
+
     result_list = []
-
     for res in results:
-        result_list.append({
-            'res': res.metrics,
-            'conf': res.config
-        })
+        result_list.append({ 'res': res.metrics, 'conf': res.config })
 
+    print('Saving results.')
     history = {
         'args': OmegaConf.to_yaml(args),
-        'ray-results': result_list
+        'test_stats': test_stats,
+        'ray-results': result_list,
     }
-
     with open(os.path.join(args.output_dir, 'results.json'), 'w') as f:
         json.dump(history, f)
 
