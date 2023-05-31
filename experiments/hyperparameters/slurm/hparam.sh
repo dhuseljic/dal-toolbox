f44135a61acfe140f734fc4a8ddfe6a1d60be53a
Denis Huseljic <dhuseljic@cluster.ies.uni-kassel.de>
2023-05-31 12:58:48 +0200

    updated hparam search sh

--- experiments/hyperparameters/slurm/hparam.sh
+++ experiments/hyperparameters/slurm/hparam.sh
@@ -3,14 +3,15 @@
 #SBATCH --partition=main
 #SBATCH --gres=gpu:2
 #SBATCH --cpus-per-task=32
-#SBATCH --output=/mnt/stud/work/phahn/uncertainty/logs/hyperparameters4/%A_%a.out
 #SBATCH --ntasks=1
-#SBATCH --mem=256GB
-#SBATCH --array=0-23%4
+#SBATCH --mem=128GB
+#SBATCH --array=0-23
+#SBATCH --output=~/work/logs/hyperparameters/bo/%A_%a_%x.out
+##SBATCH --output=~/work/uncertainty/logs/hyperparameters4/%A_%a.out
 date;hostname;pwd
-source ~/.bashrc
+# source ~/.bashrc
 source activate dal-toolbox
-cd /mnt/stud/work/phahn/uncertainty/dal-toolbox/experiments/hyperparameters/
+cd ~/projects/dal-toolbox/experiments/hyperparameters/
 
 # Define the range of hyperparameters 
 learning_rates=(0.001 0.01 0.01 0.1)
@@ -27,16 +28,18 @@ weight_decay=${weight_decays[$index % 4]}
 budget=${budgets[$index / 4 % 2]}
 random_seed=${random_seeds[$index / 8]}
 
-al_strategy=coreset
+al_strategy=entropy
 dataset=CIFAR10
+dataset_path=/mnt/work/deep_al/datasets
 queried_indices_json=/mnt/work/deep_al/results/hyperparameters/experiments/${dataset}/${al_strategy}/lr${learning_rate}_wd${weight_decay}/seed${random_seed}/queried_indices.json
-output_dir=/mnt/stud/work/phahn/uncertainty/output/hyperparameters4/${dataset}/${al_strategy}/budget${budget}/lr${learning_rate}_wd${weight_decay}/seed${random_seed}/
+output_dir=/mnt/work/deep_al/results/hyperparameters/BO/${dataset}/${al_strategy}/budget${budget}/lr${learning_rate}_wd${weight_decay}/seed${random_seed}/
 
 python -u hparam.py \
     queried_indices_json=$queried_indices_json \
     output_dir=$output_dir \
     random_seed=$random_seed \
-    dataset_path=/mnt/stud/work/phahn/uncertainty/dal-toolbox/data \
+    dataset=$dataset \
+    dataset_path=$dataset_path \
     num_cpus=4 \
     num_gpus=0.25 \
     budget=${budget} \
