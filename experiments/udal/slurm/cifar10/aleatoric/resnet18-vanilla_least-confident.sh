184e6a3d1719060f8753c1227427148683740fab
dhuseljic <dhuseljic@uni-kassel.de>
2023-06-20 09:45:07 +0200

    updates slurm scripts for vanilla model

--- experiments/udal/slurm/cifar10/aleatoric/resnet18-vanilla_least-confident.sh
+++ experiments/udal/slurm/cifar10/aleatoric/resnet18-vanilla_least-confident.sh
@@ -4,8 +4,8 @@
 #SBATCH --cpus-per-task=8
 #SBATCH --gres=gpu:1
 #SBATCH --partition=main
-#SBATCH --array=1-3%10
-#SBATCH --job-name=al_lc_resnet18-vanilla_cifar10
+#SBATCH --array=1-10%10
+#SBATCH --job-name=udal
 #SBATCH --output=/mnt/work/dhuseljic/logs/udal/active_learning/%A_%a__%x.log
 date;hostname;pwd
 source activate dal-toolbox
@@ -14,19 +14,16 @@ cd /mnt/home/dhuseljic/projects/dal-toolbox/experiments/udal/
 model=resnet18
 dataset=CIFAR10
 al_strat=least_confident
-n_init=100
-acq_size=100
-n_acq=19
+n_init=128
+acq_size=128
+n_acq=18
 random_seed=$SLURM_ARRAY_TASK_ID
-init_pool_file=/mnt/home/dhuseljic/projects/dal-toolbox/experiments/udal/initial_pools/CIFAR10/random_${n_init}_seed${random_seed}.json
+init_pool_file=/mnt/home/dhuseljic/projects/dal-toolbox/experiments/udal/initial_pools/${dataset}/random_${n_init}_seed${random_seed}.json
 output_dir=/mnt/work/deep_al/results/udal/active_learning/${dataset}/${model}/${al_strat}/N_INIT${n_init}__ACQ_SIZE${acq_size}__N_ACQ${n_acq}/seed${random_seed}/
 
 echo "Starting script. Writing results to ${output_dir}"
 srun python -u active_learning.py \
 	model=$model \
-	model.batch_size=32 \
-	model.optimizer.lr=0.01 \
-	model.optimizer.weight_decay=0.005 \
 	dataset=$dataset \
 	dataset_path=/mnt/work/dhuseljic/datasets \
 	al_strategy=$al_strat \
