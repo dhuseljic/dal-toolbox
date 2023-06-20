1947919d3bb1cc2b50f9fd9fe966b51942acdd6c
Yannick Nagel <ynagel@cluster.ies.uni-kassel.de>
2023-06-20 15:11:41 +0200

    Another slurm fix

--- experiments/active_learning/slurm/ynagel/runall_ssl.sh
+++ experiments/active_learning/slurm/ynagel/runall_ssl.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-sbatch ~/dal-toolbox/experiments/active_learning/slurm/ynagel/cifar10/budget_60/linear_coreset.sh &
-sbatch ~/dal-toolbox/experiments/active_learning/slurm/ynagel/cifar10/budget_60/linear_coreset_typiclust_init.sh &
-sbatch ~/dal-toolbox/experiments/active_learning/slurm/ynagel/cifar10/budget_60/linear_entropy.sh &
-sbatch ~/dal-toolbox/experiments/active_learning/slurm/ynagel/cifar10/budget_60/linear_entropy_typiclust_init.sh &
-sbatch ~/dal-toolbox/experiments/active_learning/slurm/ynagel/cifar10/budget_60/linear_random.sh &
-sbatch ~/dal-toolbox/experiments/active_learning/slurm/ynagel/cifar10/budget_60/linear_random_typiclust_init.sh &
-sbatch ~/dal-toolbox/experiments/active_learning/slurm/ynagel/cifar10/budget_60/linear_typiclust.sh &
-sbatch ~/dal-toolbox/experiments/active_learning/slurm/ynagel/cifar10/budget_60/linear_typiclust_typiclust_init.sh &
+sbatch ~/dal-toolbox/experiments/active_learning/slurm/ynagel/cifar10/budget_60_ssl/linear_coreset.sh &
+sbatch ~/dal-toolbox/experiments/active_learning/slurm/ynagel/cifar10/budget_60_ssl/linear_coreset_typiclust_init.sh &
+sbatch ~/dal-toolbox/experiments/active_learning/slurm/ynagel/cifar10/budget_60_ssl/linear_entropy.sh &
+sbatch ~/dal-toolbox/experiments/active_learning/slurm/ynagel/cifar10/budget_60_ssl/linear_entropy_typiclust_init.sh &
+sbatch ~/dal-toolbox/experiments/active_learning/slurm/ynagel/cifar10/budget_60_ssl/linear_random.sh &
+sbatch ~/dal-toolbox/experiments/active_learning/slurm/ynagel/cifar10/budget_60_ssl/linear_random_typiclust_init.sh &
+sbatch ~/dal-toolbox/experiments/active_learning/slurm/ynagel/cifar10/budget_60_ssl/linear_typiclust.sh &
+sbatch ~/dal-toolbox/experiments/active_learning/slurm/ynagel/cifar10/budget_60_ssl/linear_typiclust_typiclust_init.sh &
 
 
 wait
