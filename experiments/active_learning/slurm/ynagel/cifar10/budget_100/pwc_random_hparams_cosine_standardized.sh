efac656d364595f5f0c0b06e2769f6e2de85a6d9
Yannick Nagel <ynagel@cluster.ies.uni-kassel.de>
2023-07-04 20:41:43 +0200

    Added some more Hyperparameter experiments

---
+++ experiments/active_learning/slurm/ynagel/cifar10/budget_100/pwc_random_hparams_cosine_standardized.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/zsh
+#SBATCH --mem=32gb
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --partition=main
+#SBATCH --array=0-9
+#SBATCH --job-name=xpal_hparams
+#SBATCH --output=/mnt/stud/home/ynagel/logs/xpal_hparams/%A_%a__%x.log
+
+date;hostname;pwd
+source /mnt/stud/home/ynagel/dal-toolbox/venv/bin/activate
+cd ~/dal-toolbox/experiments/active_learning/
+
+model=pwc
+dataset=CIFAR10
+kernel=cosine
+
+al_strat=random
+n_init=10
+acq_size=10
+n_acq=9
+budget=$((n_init + n_acq * acq_size))
+random_seed=$SLURM_ARRAY_TASK_ID
+output_dir=/mnt/stud/home/ynagel/dal-toolbox/results/xpal_hparams/${dataset}_standardized/${model}/${al_strat}/budget_${budget}/${kernel}/seed${random_seed}/
+
+srun python -u active_learning.py \
+	model=$model \
+	model.kernel.name=$kernel \
+	dataset=$dataset \
+	dataset_path=/mnt/stud/home/ynagel/data \
+	al_strategy=$al_strat \
+	al_cycle.n_init=$n_init \
+	al_cycle.acq_size=$acq_size \
+	al_cycle.n_acq=$n_acq \
+	random_seed=$random_seed \
+	output_dir=$output_dir \
+	precomputed_features=True \
+	standardize_precomputed_features=True \
+	precomputed_features_dir=/mnt/stud/home/ynagel/data/resnet18_cifar10_87_leacc.pth
