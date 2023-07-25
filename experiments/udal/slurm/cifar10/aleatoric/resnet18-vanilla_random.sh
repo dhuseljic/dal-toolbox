abfd8bc85ec5854bf049792c85c404059997718d
dhuseljic <dhuseljic@uni-kassel.de>
2023-07-25 11:23:06 +0200

    updates slurm scripts

--- experiments/udal/slurm/cifar10/aleatoric/resnet18-vanilla_random.sh
+++ experiments/udal/slurm/cifar10/aleatoric/resnet18-vanilla_random.sh
@@ -8,7 +8,7 @@
 #SBATCH --job-name=udal
 #SBATCH --output=/mnt/work/dhuseljic/logs/udal/active_learning/%A_%a__%x.log
 date;hostname;pwd
-source activate dal-toolbox
+source ~/envs/dal-toolbox/bin/activate
 cd /mnt/home/dhuseljic/projects/dal-toolbox/experiments/udal/
 
 model=resnet18
