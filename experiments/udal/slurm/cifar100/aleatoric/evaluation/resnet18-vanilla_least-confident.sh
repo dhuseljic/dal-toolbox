03326b30a4933ef6c86d645976403af7cb28ad7b
Yannick Nagel <yannick.nagel@pm.me>
2023-08-11 11:43:43 +0200

    Corrected some CIFAR100 evaluation scripts

--- experiments/udal/slurm/cifar100/aleatoric/evaluation/resnet18-vanilla_least-confident.sh
+++ experiments/udal/slurm/cifar100/aleatoric/evaluation/resnet18-vanilla_least-confident.sh
@@ -18,15 +18,12 @@ n_init=128
 acq_size=128
 n_acq=38
 random_seed=$SLURM_ARRAY_TASK_ID
-queried_indices_json=/mnt/work/deep_al/results/udal/active_learning/${dataset}/${model}/${al_strat}/N_INIT${n_init}__ACQ_SIZE${acq_size}__N_ACQ${n_acq}/seed${random_seed}/queried_indices.json
-output_dir=/mnt/work/deep_al/results/udal/evaluation/${dataset}/${model}/${al_strat}/N_INIT${n_init}__ACQ_SIZE${acq_size}__N_ACQ${n_acq}/seed${random_seed}/
+queried_indices_json=/mnt/stud/home/ynagel/dal-toolbox/results/udal/active_learning/${dataset}/${model}/${al_strat}/N_INIT${n_init}__ACQ_SIZE${acq_size}__N_ACQ${n_acq}/seed${random_seed}/queried_indices.json
+output_dir=/mnt/stud/home/ynagel/dal-toolbox/results/udal/evaluation/${dataset}/${model}/${al_strat}/N_INIT${n_init}__ACQ_SIZE${acq_size}__N_ACQ${n_acq}/seed${random_seed}/
 
 echo "Starting script. Writing results to ${output_dir}"
 srun python -u evaluate.py \
 	model=resnet18 \
-	model.batch_size=32 \
-	model.optimizer.lr=0.01 \
-	model.optimizer.weight_decay=0.005 \
 	dataset=$dataset \
 	dataset_path=/mnt/stud/home/ynagel/data \
 	queried_indices_json=$queried_indices_json \
