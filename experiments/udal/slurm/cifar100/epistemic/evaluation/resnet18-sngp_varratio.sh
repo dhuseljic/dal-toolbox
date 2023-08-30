eab7f2581b9581e5af16c7c78ded5acdee1026ca
dhuseljic <dhuseljic@uni-kassel.de>
2023-08-30 09:43:44 +0200

    adds eval scripts

--- experiments/udal/slurm/cifar100/epistemic/evaluation/resnet18-sngp_varratio.sh
+++ experiments/udal/slurm/cifar100/epistemic/evaluation/resnet18-sngp_varratio.sh
@@ -6,26 +6,28 @@
 #SBATCH --partition=main
 #SBATCH --array=1-10%10
 #SBATCH --job-name=udal
-#SBATCH --output=/mnt/stud/home/ynagel/logs/udal/evaluation/%A_%a__%x.log
+#SBATCH --output=/mnt/work/dhuseljic/logs/udal/evaluation/%A_%a__%x.log
 date;hostname;pwd
-source /mnt/stud/home/ynagel/dal-toolbox/venv/bin/activate
-cd /mnt/stud/home/ynagel/dal-toolbox/experiments/udal/
+source ~/envs/dal-toolbox/bin/activate
+cd /mnt/home/dhuseljic/projects/dal-toolbox/experiments/udal/
 
 dataset=CIFAR100
+ood_datasets=\[CIFAR10,\ SVHN\]
 model=resnet18_sngp
 al_strat=variation_ratio
-n_init=128
-acq_size=128
-n_acq=38
+n_init=2048
+acq_size=2048
+n_acq=9
 random_seed=$SLURM_ARRAY_TASK_ID
-queried_indices_json=/mnt/stud/home/ynagel/dal-toolbox/results/udal/active_learning/${dataset}/${model}/${al_strat}/N_INIT${n_init}__ACQ_SIZE${acq_size}__N_ACQ${n_acq}/seed${random_seed}/queried_indices.json
-output_dir=/mnt/stud/home/ynagel/dal-toolbox/results/udal/evaluation/${dataset}/${model}/${al_strat}/N_INIT${n_init}__ACQ_SIZE${acq_size}__N_ACQ${n_acq}/seed${random_seed}/
+queried_indices_json=/mnt/work/dhuseljic/results/udal/active_learning/${dataset}/${model}/${al_strat}/N_INIT${n_init}__ACQ_SIZE${acq_size}__N_ACQ${n_acq}/seed${random_seed}/queried_indices.json
+output_dir=/mnt/work/dhuseljic/results/udal/evaluation/${dataset}/${model}_sngp/${al_strat}/N_INIT${n_init}__ACQ_SIZE${acq_size}__N_ACQ${n_acq}/seed${random_seed}/
 
 echo "Starting script. Writing results to ${output_dir}"
 srun python -u evaluate.py \
-	model=resnet18 \
+	model=resnet18_sngp \
 	dataset=$dataset \
-	dataset_path=/mnt/stud/home/ynagel/data \
+	ood_datasets=$ood_datasets \
+	dataset_path=/mnt/work/dhuseljic/datasets \
 	queried_indices_json=$queried_indices_json \
 	output_dir=$output_dir \
 	random_seed=$random_seed 
