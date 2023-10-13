bb33c1358bab3a5101fb373a613bfa9bba798265
Yannick Nagel <yannick.nagel@pm.me>
2023-10-13 13:15:12 +0200

    Removed unneccessary parameter

--- experiments/active_learning/active_learning.py
+++ experiments/active_learning/active_learning.py
@@ -325,13 +325,13 @@ def build_dataset(args):
     elif args.dataset.name == 'SVHN':
         data = datasets.SVHN(args.dataset_path)
     elif args.dataset.name == 'ImageNet':
-        data = datasets.ImageNet(args.dataset.path)
+        data = datasets.ImageNet(args.dataset_path)
     elif args.dataset.name == 'ImageNet50':
-        data = datasets.ImageNet50(args.dataset.path)
+        data = datasets.ImageNet50(args.dataset_path)
     elif args.dataset.name == 'ImageNet100':
-        data = datasets.ImageNet100(args.dataset.path)
+        data = datasets.ImageNet100(args.dataset_path)
     elif args.dataset.name == 'ImageNet200':
-        data = datasets.ImageNet200(args.dataset.path)
+        data = datasets.ImageNet200(args.dataset_path)
     else:
         raise NotImplementedError(f"Dataset {args.dataset.name} is not implemented!")
 
