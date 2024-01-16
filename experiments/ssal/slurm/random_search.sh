f7e00c2de6873dae9611c7e74de8624dd80b624d
Denis Huseljic <dhuseljic@cluster.ies.uni-kassel.de>
2024-01-16 10:50:44 +0100

    updates random search script

--- experiments/ssal/slurm/random_search.sh
+++ experiments/ssal/slurm/random_search.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=random_search
 #SBATCH --partition=main
-#SBATCH --output=/mnt/work/dhuseljic/logs/bayesian_updating/%A_%a_%x.log
+#SBATCH --output=/mnt/work/dhuseljic/logs/ssal/%A_%a_%x.log
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=32gb
@@ -10,13 +10,20 @@
 
 source /mnt/home/dhuseljic/envs/dal-toolbox/bin/activate
 
-# Example: Randomly pick values in the log space
+OPT=SGD
 LR=$(python -c "import numpy as np; print(10**(np.random.uniform(np.log10(0.001), np.log10(0.1))))") 
 WD=$(python -c "import numpy as np; print(10**(np.random.uniform(np.log10(0.001), np.log10(1))))") 
 
-output_dir=/mnt/work/dhuseljic/results/udal/active_learning/${dataset}/${model}/${al_strat}/N_INIT${n_init}__ACQ_SIZE${acq_size}__N_ACQ${n_acq}/seed${random_seed}/
+output_dir=/mnt/work/dhuseljic/results/ssal/test_dir
 mlflow_dir=/mnt/work/dhuseljic/mlflow/ssal/ablation
 
 cd /mnt/home/dhuseljic/projects/dal-toolbox/experiments/ssal/
 
-python ablation.py optimizer.name=AdamW optimizer.lr=$LR optimizer.weight_decay=$WD dataset_path=/mnt/work/dhuseljic/datasets output_dir=$output_dir mlflow_dir=$mlflow_dir
+srun python ablation.py \
+	optimizer.name=$OPT \
+	optimizer.lr=$LR \
+	optimizer.weight_decay=$WD \
+	model.scale_random_features=True \
+	output_dir=$output_dir \
+	mlflow_dir=$mlflow_dir\
+	dataset_path=/mnt/work/dhuseljic/datasets
