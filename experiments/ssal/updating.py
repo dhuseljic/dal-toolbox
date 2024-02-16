c7b8e535cd39a6404194beb31c748890bc9430c4
Denis Huseljic <dhuseljic@uni-kassel.de>
2024-02-16 11:12:15 +0100

    added multiple sample updates

--- experiments/ssal/updating.py
+++ experiments/ssal/updating.py
@@ -31,12 +31,9 @@ def main(args):
     data = build_data(args)
     ood_data = build_ood_data(args)
 
-    train_ds = DinoFeatureDataset(dino_model=dino_model, dataset=data.train_dataset,
-                                  normalize_features=True, cache=True)
-    test_ds = DinoFeatureDataset(dino_model=dino_model, dataset=data.test_dataset,
-                                 normalize_features=True, cache=True)
-    ood_ds = DinoFeatureDataset(dino_model=dino_model, dataset=ood_data.test_dataset,
-                                normalize_features=True, cache=True)
+    train_ds = DinoFeatureDataset(dino_model, dataset=data.train_dataset, cache=True)
+    test_ds = DinoFeatureDataset(dino_model, dataset=data.test_dataset, cache=True)
+    ood_ds = DinoFeatureDataset(dino_model, dataset=ood_data.test_dataset, cache=True)
     test_loader = DataLoader(test_ds, batch_size=args.model.predict_batch_size, shuffle=False)
 
     seed_everything(args.random_seed)
@@ -45,25 +42,25 @@ def main(args):
 
     # Define indices for training, updating and retraining
     rnd_indices = torch.randperm(len(train_ds))
-    train_indices = rnd_indices[:args.num_init_samples]
-    new_indices = rnd_indices[args.num_init_samples:args.num_init_samples+args.num_new_samples]
-    retrain_indices = rnd_indices[:args.num_init_samples+args.num_new_samples]
+    base_indices = rnd_indices[:args.num_init_samples]
+    new_indices = rnd_indices[args.num_init_samples:args.num_init_samples+max(args.num_new_samples)]
+    retrain_indices = rnd_indices[:args.num_init_samples+max(args.num_new_samples)]
 
     # OOD eval datasets
-    num_samples_per_init = args.num_ood_samples // args.num_init_samples
-    dist = pairwise_distances(train_ds.features[train_indices], train_ds.features)
-    id_indices = dist.argsort(axis=-1)[:, 1:num_samples_per_init+1].flatten()
-    ood_indices = range(len(id_indices))
-    id_loader = DataLoader(train_ds, sampler=id_indices, batch_size=args.model.train_batch_size)
-    ood_loader = DataLoader(ood_ds, sampler=ood_indices, batch_size=args.model.train_batch_size)
+    # num_samples_per_init = args.num_ood_samples // args.num_init_samples
+    # dist = pairwise_distances(train_ds.features[base_indices], train_ds.features)
+    # id_indices = dist.argsort(axis=-1)[:, 1:num_samples_per_init+1].flatten()
+    # ood_indices = range(len(id_indices))
+    # id_loader = DataLoader(train_ds, sampler=id_indices, batch_size=args.model.train_batch_size)
+    # ood_loader = DataLoader(ood_ds, sampler=ood_indices, batch_size=args.model.train_batch_size)
 
     # Train
     base_model = copy.deepcopy(init_model)
     train_loader = DataLoader(
-        Subset(train_ds, indices=train_indices),
+        Subset(train_ds, indices=base_indices),
         batch_size=args.model.train_batch_size,
         shuffle=True,
-        drop_last=len(train_indices) > args.model.train_batch_size,
+        drop_last=len(base_indices) > args.model.train_batch_size,
     )
     trainer = Trainer(
         max_epochs=args.model.num_epochs,
@@ -79,11 +76,10 @@ def main(args):
     predictions_base = trainer.predict(base_model, test_loader)
     test_stats_base = evaluate(predictions_base)
     y_pred_original = torch.cat([pred[0] for pred in predictions_base]).argmax(-1)
-
-    id_predictions = trainer.predict(base_model, id_loader)
-    ood_predictions = trainer.predict(base_model, ood_loader)
-    ood_stats = evaluate_ood(id_predictions, ood_predictions)
-    test_stats_base.update(ood_stats)
+    # id_predictions = trainer.predict(base_model, id_loader)
+    # ood_predictions = trainer.predict(base_model, ood_loader)
+    # ood_stats = evaluate_ood(id_predictions, ood_predictions)
+    # test_stats_base.update(ood_stats)
 
     if args.model.name == 'deterministic':
         print('Base model:', test_stats_base)
@@ -91,61 +87,60 @@ def main(args):
         mlflow.end_run()
         return
 
-    # Updating
-    update_model = copy.deepcopy(base_model)
-    update_loader = DataLoader(Subset(train_ds, indices=new_indices), batch_size=args.model.train_batch_size,)
-    start_time = time.time()
-    update_model.update_posterior(update_loader, lmb=args.update_lmb,
-                                  gamma=args.update_gamma, likelihood=args.likelihood)
-    updating_time = time.time() - start_time
+    for num_new in args.num_new_samples:
+        # Updating
+        update_model = copy.deepcopy(base_model)
+        update_ds = Subset(train_ds, indices=new_indices[:num_new])
+        update_loader = DataLoader(update_ds, batch_size=args.model.train_batch_size)
+        start_time = time.time()
+        update_model.update_posterior(update_loader, lmb=args.update_lmb,
+                                      gamma=args.update_gamma, likelihood=args.likelihood)
+        updating_time = time.time() - start_time
 
-    predictions_updated = trainer.predict(update_model, test_loader)
-    test_stats_updating = evaluate(predictions_updated)
+        predictions_updated = trainer.predict(update_model, test_loader)
+        test_stats_updating = evaluate(predictions_updated)
+        y_pred_updated = torch.cat([pred[0] for pred in predictions_updated]).argmax(-1)
+        test_stats_updating['decision_flips'] = torch.sum(y_pred_original != y_pred_updated).item()
+        test_stats_updating['updating_time'] = updating_time
+        # id_predictions = trainer.predict(update_model, id_loader)
+        # ood_predictions = trainer.predict(update_model, ood_loader)
+        # ood_stats = evaluate_ood(id_predictions, ood_predictions)
+        # test_stats_updating.update(ood_stats)
 
-    id_predictions = trainer.predict(update_model, id_loader)
-    ood_predictions = trainer.predict(update_model, ood_loader)
-    ood_stats = evaluate_ood(id_predictions, ood_predictions)
-    test_stats_updating.update(ood_stats)
+        # Retraining
+        retrain_model = copy.deepcopy(init_model)
+        retrain_ds = Subset(train_ds, indices=retrain_indices[:args.num_init_samples+num_new])
+        retrain_loader = DataLoader(retrain_ds, batch_size=args.model.train_batch_size,
+                                    shuffle=True, drop_last=len(base_indices) > args.model.train_batch_size)
 
-    y_pred_updated = torch.cat([pred[0] for pred in predictions_updated]).argmax(-1)
-    test_stats_updating['decision_flips'] = torch.sum(y_pred_original != y_pred_updated).item()
-    test_stats_updating['updating_time'] = updating_time
+        trainer = Trainer(
+            max_epochs=args.model.num_epochs,
+            default_root_dir=args.output_dir,
+            enable_checkpointing=False,
+            logger=False,
+            enable_progress_bar=False,
+            callbacks=[MetricLogger()],
+        )
+        start_time = time.time()
+        trainer.fit(retrain_model, train_dataloaders=retrain_loader)
+        retraining_time = time.time() - start_time
 
-    # Retraining
-    retrain_model = copy.deepcopy(init_model)
-    train_loader = DataLoader(
-        Subset(train_ds, indices=retrain_indices),
-        batch_size=args.model.train_batch_size,
-        shuffle=True,
-        drop_last=len(train_indices) > args.model.train_batch_size,
-    )
-    trainer = Trainer(
-        max_epochs=args.model.num_epochs,
-        default_root_dir=args.output_dir,
-        enable_checkpointing=False,
-        logger=False,
-        enable_progress_bar=False,
-        callbacks=[MetricLogger()],
-    )
-    start_time = time.time()
-    trainer.fit(retrain_model, train_dataloaders=train_loader)
-    retraining_time = time.time() - start_time
+        predictions_retrained = trainer.predict(retrain_model, test_loader)
+        test_stats_retraining = evaluate(predictions_retrained)
+        y_pred_retrained = torch.cat([pred[0] for pred in predictions_retrained]).argmax(-1)
+        test_stats_retraining['decision_flips'] = torch.sum(y_pred_original != y_pred_retrained).item()
+        test_stats_retraining['retraining_time'] = retraining_time
 
-    predictions_retrained = trainer.predict(retrain_model, test_loader)
-    test_stats_retraining = evaluate(predictions_retrained)
+        # id_predictions = trainer.predict(retrain_model, id_loader)
+        # ood_predictions = trainer.predict(retrain_model, ood_loader)
+        # ood_stats = evaluate_ood(id_predictions, ood_predictions)
+        # test_stats_retraining.update(ood_stats)
 
-    id_predictions = trainer.predict(retrain_model, id_loader)
-    ood_predictions = trainer.predict(retrain_model, ood_loader)
-    ood_stats = evaluate_ood(id_predictions, ood_predictions)
-    test_stats_retraining.update(ood_stats)
-
-    y_pred_retrained = torch.cat([pred[0] for pred in predictions_retrained]).argmax(-1)
-    test_stats_retraining['decision_flips'] = torch.sum(y_pred_original != y_pred_retrained).item()
-    test_stats_retraining['retraining_time'] = retraining_time
-
-    print('Base model:', test_stats_base)
-    print('Updated model:', test_stats_updating)
-    print('Retrained model:', test_stats_retraining)
+        print('Number of new samples:', num_new)
+        print('Base model:', test_stats_base)
+        print('Updated model:', test_stats_updating)
+        print('Retrained model:', test_stats_retraining)
+        print('=' * 20)
 
     mlflow.log_metrics({f'base_{k}': v for k, v in test_stats_base.items()})
     mlflow.log_metrics({f'updated_{k}': v for k, v in test_stats_updating.items()})
@@ -177,6 +172,60 @@ def evaluate_ood(id_predictions, ood_predictions):
         'AUPR': metrics.OODAUPR()(id_entropy, ood_entropy).item()
     }
     return ood_stats
+
+
+def update_mc():
+    new_model = deepcopy(model)
+    update_loader = DataLoader(new_ds, batch_size=256)
+
+    phis_list = []
+    targets_list = []
+    for inputs, targets in update_loader:
+        with torch.no_grad():
+            logits, phis = new_model.model(inputs, return_random_features=True)
+    phis_list.append(phis)
+    targets_list.append(targets)
+    phis = torch.cat(phis_list)
+    targets = torch.cat(targets_list)
+    num_classes = logits.size(-1)
+
+    # Sample hypothesis
+    for m in new_model.model.modules():
+        if isinstance(m, RandomFeatureGaussianProcess):
+            gp = m
+    mean = gp.beta.weight.data
+    cov = gp.covariance_matrix.data
+    torch.manual_seed(1)
+    dist = torch.distributions.MultivariateNormal(loc=mean, covariance_matrix=cov)
+    sampled_params = dist.sample(sample_shape=(50000,))
+
+    logits_mc = torch.einsum('nd,ekd->nek', phis, sampled_params)
+    log_probas_mc = logits_mc.log_softmax(-1)
+
+    n_samples, n_members, n_classes = logits_mc.shape
+    # Define uniform prior
+    log_prior = torch.log(torch.ones(n_members) / n_members)  # uniform prior
+    # Compute log likelihood
+    log_likelihood = log_probas_mc.permute(1, 0, 2)[:, range(n_samples), targets].sum(dim=1)
+    # Compute log posterior
+    log_posterior = log_prior + log_likelihood
+    weights = torch.exp(log_posterior - log_posterior.max())  # normalize log probs numerically stable
+    weights /= weights.sum()
+
+    with torch.no_grad():
+        _, zz_phi = new_model.model(zz, return_random_features=True)
+    logits_mc = torch.einsum('nd,ekd->nek', zz_phi, sampled_params)
+    probas_mc = logits_mc.softmax(-1)
+    probas = torch.einsum('e,nek->nk', weights, probas_mc)
+    probas = probas[:, 1]
+
+    plt.scatter(X[:, 0], X[:, 1], c=y)
+    plt.scatter(X_new[:, 0], X_new[:, 1], c=y_new)
+    plt.contour(xx, yy, probas.reshape(xx.shape), levels=[0.5], colors='k')
+    plt.contourf(xx, yy, probas.reshape(xx.shape), cmap='coolwarm', levels=np.linspace(0, 1, 6))
+    plt.colorbar()
+    plt.scatter(X[:, 0], X[:, 1], c=y)
+    plt.scatter(X_new[:, 0], X_new[:, 1], c=y_new)
 
 
 if __name__ == '__main__':
--- experiments/ssal/utils.py
+++ experiments/ssal/utils.py
@@ -237,7 +237,8 @@ def build_model(args, **kwargs):
     elif args.model.name == 'laplace':
         model = LaplaceNet(num_features, num_classes,
                            mean_field_factor=args.model.mean_field_factor, mc_samples=args.model.mc_samples)
-        if args.al.strategy in ['bald', 'pseudo_bald', 'batch_bald']:
+        
+        if 'al' in args and args.al.strategy in ['bald', 'pseudo_bald', 'batch_bald']:
             LaplaceNet.use_mean_field = False
     elif args.model.name == 'deterministic':
         model = DeterministcNet(num_features, num_classes)
