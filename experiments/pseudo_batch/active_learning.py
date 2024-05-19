8ebd527d065ea5d5714c50bb427d8f765d5dee49
Denis Huseljic <dhuseljic@uni-kassel.de>
2024-05-19 18:02:46 +0200

    added bald

--- experiments/pseudo_batch/active_learning.py
+++ experiments/pseudo_batch/active_learning.py
@@ -113,28 +113,12 @@ def build_al_strategy(args):
         strat = strategies.Badge(subset_size=args.al.subset_size)
         al_strategy = PseudoBatch(al_strategy=strat, update_every=args.update_every,
                                   gamma=args.update_gamma, subset_size=args.al.subset_size)
-    elif args.al.strategy == 'batch_bald':
-        al_strategy = strategies.BatchBALDSampling(subset_size=args.al.subset_size)
+    elif args.al.strategy == 'bald':
+        al_strategy = strategies.BALDSampling(subset_size=args.al.subset_size)
     elif args.al.strategy == 'pseudo_bald':
         strat = strategies.BALDSampling(subset_size=args.al.subset_size)
         al_strategy = PseudoBatch(al_strategy=strat, update_every=args.update_every,
                                   gamma=args.update_gamma, subset_size=args.al.subset_size)
-    elif args.al.strategy == 'bait':
-        al_strategy = strategies.BaitSampling(
-            subset_size=args.al.subset_size,
-            grad_likelihood=args.al.bait.grad_likelihood,
-            device=args.al.device
-        )
-    elif args.al.strategy == 'pseudo_bait':
-        strat = strategies.BaitSampling(
-            subset_size=args.al.subset_size,
-            grad_likelihood=args.al.bait.grad_likelihood,
-            device=args.al.device
-        )
-        al_strategy = PseudoBatch(al_strategy=strat, update_every=args.update_every,
-                                  gamma=args.update_gamma, subset_size=args.al.subset_size)
-    elif args.al.strategy == 'typiclust':
-        al_strategy = strategies.TypiClust(subset_size=args.al.subset_size)
     else:
         raise NotImplementedError()
     return al_strategy
