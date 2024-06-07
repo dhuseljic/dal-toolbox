1c7e223eeca00e7eaadf8420f52b0c3050fe55bc
Denis Huseljic <dhuseljic@cluster.ies.uni-kassel.de>
2024-06-07 15:17:44 +0200

    udpated slurm scripts

--- experiments/pseudo_batch/slurm/active_learning.sh
+++ experiments/pseudo_batch/slurm/active_learning.sh
@@ -1,53 +1,63 @@
 #!/bin/bash
 #SBATCH --job-name=active_learning
 #SBATCH --partition=main
-#SBATCH --output=/mnt/work/dhuseljic/logs/ssal/active_learning/%A_%a_%x.log
+#SBATCH --output=/mnt/work/dhuseljic/logs/pseudo_batch/active_learning/%A_%a_%x.log
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=32gb
 #SBATCH --gres=gpu:0
-#SBATCH --array=1-50
+#SBATCH --array=11-20
 #SBATCH --exclude=gpu-a100-[1-5],gpu-v100-[1-4]
 source /mnt/home/dhuseljic/envs/dal-toolbox/bin/activate
+export HF_HOME=/mnt/work/dhuseljic/hugging_face_cache/misc
+export HF_DATASETS_CACHE==/mnt/work/dhuseljic/hugging_face_cache/datasets
+export TRANSFORMERS_CACHE=/mnt/work/dhuseljic/hugging_face_cache/models
 
-DATASET=cifar100
+DATASET=cifar10
+USE_VAL=False
 DINO_MODEL=dinov2_vits14
 
-INIT_METHOD=diverse_dense
-STRAT=typiclust
-NUM_INIT=100
+INIT_METHOD=random
+STRAT=badge
+NUM_INIT=10
 NUM_ACQ=20
-ACQ_SIZE=100
-SUBSET_SIZE=10000
+ACQ_SIZE=10
+SUBSET_SIZE=1000
 
 MODEL=laplace
 NUM_EPOCHS=200
-SCALE_FEATURES=True
 LIKELIHOOD=gaussian
 LMB=1
-GAMMA=10
+GAMMA=15
 SEED=$SLURM_ARRAY_TASK_ID
 
-mlflow_dir=/mnt/work/dhuseljic/mlflow/ssal/al/${DATASET}
+# mlflow_uri=sqlite:////mnt/work/dhuseljic/mlflow/pseudo_batch/${DATASET}_al_benchmark.db
+# mlflow_uri=sqlite:////mnt/work/dhuseljic/mlflow/pseudo_batch/${DATASET}_al_ablation.db
+mlflow_uri=sqlite:////mnt/home/dhuseljic/mlflow/pseudo_batch/${DATASET}_al_ablation.db
+# mlflow_uri=file:////mnt/home/dhuseljic/mlflow/pseudo_batch/${DATASET}_al_ablation/
+if [ "$SEED" -eq 1 ]; then
+	python -c "import mlflow; mlflow.set_tracking_uri(r'$mlflow_uri'); mlflow.set_experiment('Active Learning')"
+fi
 
-cd /mnt/home/dhuseljic/projects/dal-toolbox/experiments/ssal/
+cd /mnt/home/dhuseljic/projects/dal-toolbox/experiments/pseudo_batch/
 srun python active_learning.py \
 	dataset_name=$DATASET \
 	dataset_path=/mnt/work/dhuseljic/datasets \
+	feature_cache_dir=/mnt/work/dhuseljic/datasets \
+	use_val_split=$USE_VAL \
 	dino_model_name=$DINO_MODEL \
-	dino_cache_dir=/mnt/work/dhuseljic/datasets \
 	al.strategy=$STRAT \
 	al.init_method=$INIT_METHOD \
 	al.num_init_samples=$NUM_INIT \
 	al.num_acq=$NUM_ACQ \
 	al.acq_size=$ACQ_SIZE \
 	al.subset_size=$SUBSET_SIZE \
+	al.bait.grad_likelihood=binary_cross_entropy \
 	model.name=$MODEL \
 	model.num_epochs=$NUM_EPOCHS \
-	model.scale_random_features=$SCALE_FEATURES \
 	likelihood=$LIKELIHOOD \
 	update_lmb=$LMB \
 	update_gamma=$GAMMA \
-	mlflow_dir=$mlflow_dir \
+	mlflow_uri=$mlflow_uri \
 	random_seed=$SEED \
-	output_dir=/mnt/work/dhuseljic/results/ssal/test_dir
+	output_dir=/mnt/work/dhuseljic/results/pseudo_batch/test_dir
--- experiments/pseudo_batch/slurm/updating.sh
+++ experiments/pseudo_batch/slurm/updating.sh
@@ -1,45 +1,63 @@
 #!/bin/bash
 #SBATCH --job-name=updating
 #SBATCH --partition=main
-#SBATCH --output=/mnt/work/dhuseljic/logs/ssal/%A_%a_%x.log
+#SBATCH --output=/mnt/work/dhuseljic/logs/pseudo_batch/updating/%A_%a_%x.log
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=32gb
 #SBATCH --gres=gpu:0
 #SBATCH --exclude=gpu-a100-[1-5],gpu-v100-[1-4]
-#SBATCH --array=1-100
-
+#SBATCH --array=1-1
 source /mnt/home/dhuseljic/envs/dal-toolbox/bin/activate
+export HF_HOME=/mnt/work/dhuseljic/hugging_face_cache/misc
+export HF_DATASETS_CACHE==/mnt/work/dhuseljic/hugging_face_cache/datasets
+export TRANSFORMERS_CACHE=/mnt/work/dhuseljic/hugging_face_cache/models
 
-DATASET=food101
-OOD_DATASET=cifar10
-NUM_INIT=100
-NUM_NEW=100
+DATASET=banking77
+LR=1e-1
+mlflow_uri=file:////mnt/home/dhuseljic/mlflow/pseudo_batch/${DATASET}_gamma_ablation/
+# mlflow_uri=file:////mnt/home/dhuseljic/mlflow/pseudo_batch/${DATASET}_initds/
+# mlflow_uri=file:////mnt/home/dhuseljic/mlflow/pseudo_batch/${DATASET}_newds/
+
+GAMMA=0.05
+GAMMA_FO=0.005
+GAMMA_MC=0.05
+MC_SAMPLES=5
+
+NUM_INIT=50
+NUM_NEW="$(python -c 'print(str(list(range(1, 10+1, 1))).replace(" ", ""))')"
+
+USE_VAL_SPLIT=True
 LMB=1
-GAMMA=1
-LIKELIHOOD=gaussian
-MODEL=laplace
-NUM_EPOCHS=500
-DINO_MODEL=dinov2_vitl14
+LIKELIHOOD=categorical
+NUM_EPOCHS=200
+DINO_MODEL=dinov2_vits14
 SEED=$SLURM_ARRAY_TASK_ID
-SCALE_FEATURES=True
 
-mlflow_dir=/mnt/work/dhuseljic/mlflow/ssal/updating_${DATASET}
+experiment_name=updating
 
-cd /mnt/home/dhuseljic/projects/dal-toolbox/experiments/ssal/
+if [ "$SEED" -eq 1 ]; then
+	python -c "import mlflow; mlflow.set_tracking_uri(r'$mlflow_uri'); mlflow.set_experiment('$experiment_name')"
+fi
+
+cd /mnt/home/dhuseljic/projects/dal-toolbox/experiments/pseudo_batch/
 srun python updating.py \
+	dataset_name=$DATASET \
+	dataset_path=/mnt/work/dhuseljic/datasets \
+	use_val_split=$USE_VAL_SPLIT \
+	feature_cache_dir=/mnt/work/dhuseljic/datasets \
+	output_dir=/mnt/work/dhuseljic/results/test_dir \
 	dino_model_name=$DINO_MODEL \
 	num_init_samples=$NUM_INIT \
 	num_new_samples=$NUM_NEW \
 	update_lmb=$LMB \
 	update_gamma=$GAMMA \
+	update_gamma_fo=$GAMMA_FO \
+	update_gamma_mc=$GAMMA_MC \
 	likelihood=$LIKELIHOOD \
-	model.name=$MODEL \
 	model.num_epochs=$NUM_EPOCHS \
-	model.scale_random_features=$SCALE_FEATURES \
-	mlflow_dir=$mlflow_dir \
-	random_seed=$SEED \
-	output_dir=/mnt/work/dhuseljic/results/ssal/test_dir \
-	dataset_name=$DATASET \
-	ood_dataset_name=$OOD_DATASET \
-	dataset_path=/mnt/work/dhuseljic/datasets
+	model.mc_samples=$MC_SAMPLES \
+	optimizer.lr=$LR \
+	experiment_name=$experiment_name \
+	mlflow_uri=$mlflow_uri \
+	random_seed=$SEED
