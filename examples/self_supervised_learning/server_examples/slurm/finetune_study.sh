4a6b4349bce73f0d546454bdbdc7d2355a66eb85
nhaH-luaP <paul.hahn@uni-kassel.de>
2024-11-16 19:16:07 +0100

    prepared pretrain finetune studies

--- examples/self_supervised_learning/server_examples/slurm/finetune_study.sh
+++ examples/self_supervised_learning/server_examples/slurm/finetune_study.sh
@@ -6,7 +6,7 @@
 #SBATCH --partition=main
 #SBATCH --job-name=finetune_study
 #SBATCH --output=/mnt/stud/work/phahn/repositories/dal-toolbox/logs/self_supervised_learning/%x_%A_%a.log
-#SBATCH --array=0-29%4
+#SBATCH --array=0-39%4
 
 date;hostname;pwd
 source /mnt/stud/work/phahn/venvs/dal-toolbox/bin/activate
@@ -14,14 +14,14 @@ cd /mnt/stud/work/phahn/repositories/dal-toolbox/dal-toolbox/examples/self_super
 
 load_pretrained_weights=(False True)
 subset_sizes=(50 100 250 500 1000)
-seeds=(1 2 3 4 5)
+seeds=(1 2 3 4)
 
 index=$SLURM_ARRAY_TASK_ID
 model=resnet18
 dataset=CIFAR10
 load_pre=${load_pretrained_weights[$index % 2]}
-n_labeled=${subset_sizes[$index / 2 % 3]}
-seed=${seeds[$index / 6]}
+subset_size=${subset_sizes[$index / 2 % 5]}
+seed=${seeds[$index / 10]}
 
 OUTPUT_DIR=/mnt/stud/work/phahn/repositories/dal-toolbox/output/self_supervised_learning/finetuning/${dataset}/${model}/${load_pre}/${subset_size}/seed_${seed}/
 DATA_DIR=/mnt/stud/work/phahn/datasets/
