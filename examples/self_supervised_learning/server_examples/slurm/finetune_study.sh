6c01e703f59b01fa10c3c57aaa46cff98f8aa790
nhaH-luaP <paul.hahn@uni-kassel.de>
2024-11-17 14:27:48 +0100

    fixed finetune script

--- examples/self_supervised_learning/server_examples/slurm/finetune_study.sh
+++ examples/self_supervised_learning/server_examples/slurm/finetune_study.sh
@@ -6,26 +6,27 @@
 #SBATCH --partition=main
 #SBATCH --job-name=finetune_study
 #SBATCH --output=/mnt/stud/work/phahn/repositories/dal-toolbox/logs/self_supervised_learning/%x_%A_%a.log
-#SBATCH --array=0-39%4
+#SBATCH --array=0-89%4
 
 date;hostname;pwd
 source /mnt/stud/work/phahn/venvs/dal-toolbox/bin/activate
 cd /mnt/stud/work/phahn/repositories/dal-toolbox/dal-toolbox/examples/self_supervised_learning/server_examples/
 
+models=(resnet18 wideresnet282 wideresnet2810)
 load_pretrained_weights=(False True)
 subset_sizes=(50 100 250 500 1000)
-seeds=(1 2 3 4)
+seeds=(1 2 3)
 
 index=$SLURM_ARRAY_TASK_ID
-model=resnet18
+model=${models[$index % 3]}
 dataset=CIFAR10
-load_pre=${load_pretrained_weights[$index % 2]}
-subset_size=${subset_sizes[$index / 2 % 5]}
-seed=${seeds[$index / 10]}
+load_pre=${load_pretrained_weights[$index / 3 % 2]}
+subset_size=${subset_sizes[$index / 6 % 5]}
+seed=${seeds[$index / 30]}
 
 OUTPUT_DIR=/mnt/stud/work/phahn/repositories/dal-toolbox/output/self_supervised_learning/finetuning/${dataset}/${model}/${load_pre}/${subset_size}/seed_${seed}/
 DATA_DIR=/mnt/stud/work/phahn/datasets/
-MODEL_PATH=/mnt/stud/work/phahn/repositories/dal-toolbox/storage/${dataset}/${model}/seed_${seed}/pretrained_weights_seed_${seed}.pth
+MODEL_PATH=/mnt/stud/work/phahn/repositories/dal-toolbox/storage/${dataset}/${model}/seed_1/pretrained_weights_seed_42.pth
 echo "Saving results to $OUTPUT_DIR"
 
 srun python -u finetune.py \
