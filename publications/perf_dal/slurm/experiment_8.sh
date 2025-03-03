759da57f7ff774fe3d86ff81995bbe88d4355e8a
nhaH-luaP <paul.hahn@uni-kassel.de>
2025-03-03 12:36:19 +0100

    new experiment prepared

---
+++ publications/perf_dal/slurm/experiment_8.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+#SBATCH --job-name=optimal_al_baselines
+#SBATCH --partition=main
+#SBATCH --output=/mnt/stud/work/phahn/repositories/dal-toolbox/logs/perf_dal_new/%A_%a_%x.log
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=64gb
+#SBATCH --gres=gpu:1
+#SBATCH --array=0-159%4
+source /mnt/stud/work/phahn/venvs/dal-toolbox/bin/activate
+
+mlflow_uri='sqlite:////mnt/stud/work/phahn/repositories/dal-toolbox/perf_dal.db'
+mlflow_exp_name='experiment_8'
+al_strategy='perf_dal_oracle'
+var_sss=True
+num_batches=110
+
+selection_strategies=(\[random,bait\] \[random,bait,dropqueryclass\] \[random,dropquery,bait,dropqueryclass\] \[random,typiclust,dropquery,bait,typiclass,dropqueryclass\] \[random,typiclust,dropquery,bait,typiclass,dropqueryclass,alfamix\] \[random,typiclust,dropquery,bait,typiclass,dropqueryclass,coreset,alfamix\] \[random,typiclust,dropquery,bait,typiclass,dropqueryclass,badge,coreset,alfamix\] \[random,typiclust,dropquery,bait,typiclass,dropqueryclass,margin,badge,coreset,alfamix\])
+
+datasets=(cifar10 dtd)
+acq_sizes=(10 50)
+subset_sizes=(1000 Null)
+
+random_seeds=(1 2 3 4 5 6 7 8 9 10)
+
+index=$SLURM_ARRAY_TASK_ID
+dataset_name=${datasets[$index % 2]}
+acq_size=${acq_sizes[$index % 2]}
+subset_size=${subset_sizes[$index % 2]}
+
+sel_strats=${selection_strategies[$index / 2 % 8]}
+
+random_seed=${random_seeds[$index / 16]}
+
+if [ $index -eq 0 ]; then
+    python -c "import mlflow; mlflow.set_tracking_uri(r'$mlflow_uri'); mlflow.set_experiment(r'$mlflow_exp_name')"
+fi
+
+date;hostname
+cd /mnt/stud/work/phahn/repositories/dal-toolbox/dal-toolbox/publications/perf_dal
+srun python al.py \
+    dataset_name=$dataset_name \
+    dataset_path=/mnt/stud/work/phahn/datasets \
+    al.strategy=$al_strategy \
+    al.acq_size=$acq_size \
+    al.subset_size=$subset_size \
+    al.optimal.strategies=$sel_strats \
+    al.optimal.vary_strat_subset_size=$var_sss \
+    al.optimal.num_batches=$n_bat \
+    mlflow_uri=$mlflow_uri \
+    al.device=cuda \
+    experiment_name=$mlflow_exp_name \
+    random_seed=$random_seed \
