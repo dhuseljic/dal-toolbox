09f11e43f5ba401e47392b428676a71b43602442
nhaH-luaP <paul.hahn@uni-kassel.de>
2025-03-31 00:44:49 +0200

    prepared experiment 6 - final oracle

--- publications/perf_dal/slurm/paul/experiment_6.sh
+++ publications/perf_dal/slurm/paul/experiment_6.sh
@@ -6,31 +6,30 @@
 #SBATCH --cpus-per-task=16
 #SBATCH --mem=64gb
 #SBATCH --gres=gpu:1
-#SBATCH --array=0-59%4
+#SBATCH --array=0-89%4
 source /mnt/stud/work/phahn/venvs/dal-toolbox/bin/activate
 
-mlflow_uri='sqlite:////mnt/stud/work/phahn/repositories/dal-toolbox/perf_dal.db'
+mlflow_uri='sqlite:////mnt/stud/work/phahn/repositories/dal-toolbox/perf_dal_2.db'
 mlflow_exp_name='experiment_6'
+
 al_strategy='perf_dal_oracle'
-sel_strats=\[random,typiclust,dropquery,bait,dropqueryclass\]
+sel_strats=\[random,typiclust,dropquery,bait,typiclass,dropqueryclass,loss,coreset,alfamix\]
 var_sss=True
+n_bat=110
+training=train
+loss_fn=brier
 
-datasets=(cifar10 dtd)
-acq_sizes=(10 50)
-subset_sizes=(1000 Null)
-
-num_batches=(11 55 110)
+datasets=(cifar10 stl10 snacks flowers102 dtd food101 cifar100 tiny_imagenet imagenet)
+acq_sizes=(10 10 20 25 50 100 100 200 1000)
+subset_sizes=(1000 Null Null Null Null 1000 1000 5000 5000)
 
 random_seeds=(1 2 3 4 5 6 7 8 9 10)
 
 index=$SLURM_ARRAY_TASK_ID
-dataset_name=${datasets[$index % 2]}
-acq_size=${acq_sizes[$index % 2]}
-subset_size=${subset_sizes[$index % 2]}
-
-n_bat=${num_batches[$index / 2 % 3]}
-
-random_seed=${random_seeds[$index / 6]}
+dataset_name=${datasets[$index % 9]}
+acq_size=${acq_sizes[$index % 9]}
+subset_size=${subset_sizes[$index % 9]}
+random_seed=${random_seeds[$index / 9]}
 
 if [ $index -eq 0 ]; then
     python -c "import mlflow; mlflow.set_tracking_uri(r'$mlflow_uri'); mlflow.set_experiment(r'$mlflow_exp_name')"
@@ -44,10 +43,12 @@ srun python al.py \
     al.strategy=$al_strategy \
     al.acq_size=$acq_size \
     al.subset_size=$subset_size \
-    al.optimal.strategies=$sel_strats \
-    al.optimal.vary_strat_subset_size=$var_sss \
-    al.optimal.num_batches=$n_bat \
     mlflow_uri=$mlflow_uri \
     al.device=cuda \
     experiment_name=$mlflow_exp_name \
     random_seed=$random_seed \
+    al.optimal.vary_strat_subset_size=$var_sss \
+    al.optimal.num_batches=$n_bat \
+    al.optimal.loss=$loss_fn \
+    al.optimal.retraining=$training \
+    al.optimal.strategies=$sel_strats \
--- publications/perf_dal/slurm/paul/old/experiment_6.sh
+++ publications/perf_dal/slurm/paul/old/experiment_6.sh
@@ -6,30 +6,31 @@
 #SBATCH --cpus-per-task=16
 #SBATCH --mem=64gb
 #SBATCH --gres=gpu:1
-#SBATCH --array=0-89%4
+#SBATCH --array=0-59%4
 source /mnt/stud/work/phahn/venvs/dal-toolbox/bin/activate
 
 mlflow_uri='sqlite:////mnt/stud/work/phahn/repositories/dal-toolbox/perf_dal.db'
-mlflow_exp_name='experiment_10'
-
+mlflow_exp_name='experiment_6'
 al_strategy='perf_dal_oracle'
-sel_strats=\[random,typiclust,dropquery,bait,typiclass,dropqueryclass,badge,coreset,alfamix\]
+sel_strats=\[random,typiclust,dropquery,bait,dropqueryclass\]
 var_sss=True
-n_bat=110
-training=train
-loss_fn=brier
 
-datasets=(cifar10 stl10 snacks flowers102 dtd food101 cifar100 tiny_imagenet imagenet)
-acq_sizes=(10 10 20 25 50 100 100 200 1000)
-subset_sizes=(1000 Null Null Null Null 1000 1000 5000 5000)
+datasets=(cifar10 dtd)
+acq_sizes=(10 50)
+subset_sizes=(1000 Null)
+
+num_batches=(11 55 110)
 
 random_seeds=(1 2 3 4 5 6 7 8 9 10)
 
 index=$SLURM_ARRAY_TASK_ID
-dataset_name=${datasets[$index % 9]}
-acq_size=${acq_sizes[$index % 9]}
-subset_size=${subset_sizes[$index % 9]}
-random_seed=${random_seeds[$index / 9]}
+dataset_name=${datasets[$index % 2]}
+acq_size=${acq_sizes[$index % 2]}
+subset_size=${subset_sizes[$index % 2]}
+
+n_bat=${num_batches[$index / 2 % 3]}
+
+random_seed=${random_seeds[$index / 6]}
 
 if [ $index -eq 0 ]; then
     python -c "import mlflow; mlflow.set_tracking_uri(r'$mlflow_uri'); mlflow.set_experiment(r'$mlflow_exp_name')"
@@ -43,12 +44,10 @@ srun python al.py \
     al.strategy=$al_strategy \
     al.acq_size=$acq_size \
     al.subset_size=$subset_size \
+    al.optimal.strategies=$sel_strats \
+    al.optimal.vary_strat_subset_size=$var_sss \
+    al.optimal.num_batches=$n_bat \
     mlflow_uri=$mlflow_uri \
     al.device=cuda \
     experiment_name=$mlflow_exp_name \
     random_seed=$random_seed \
-    al.optimal.vary_strat_subset_size=$var_sss \
-    al.optimal.num_batches=$n_bat \
-    al.optimal.loss=$loss_fn \
-    al.optimal.retraining=$training \
-    al.optimal.strategies=$sel_strats \
