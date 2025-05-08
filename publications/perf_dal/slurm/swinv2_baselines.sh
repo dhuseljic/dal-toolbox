e17cb586ebed49e55e9790ab96a8481b3da8a3cf
nhaH-luaP <paul.hahn@uni-kassel.de>
2025-05-08 23:49:44 +0200

    prepared new exps

---
+++ publications/perf_dal/slurm/swinv2_baselines.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+#SBATCH --job-name=optimal_al_baselines
+#SBATCH --partition=main
+#SBATCH --output=/mnt/stud/work/phahn/repositories/dal-toolbox/logs/perf_dal_new/%A_%a_%x.log
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=64gb
+#SBATCH --gres=gpu:1
+#SBATCH --array=0-719%4
+source /mnt/stud/work/phahn/venvs/dal-toolbox/bin/activate
+
+mlflow_uri='sqlite:////mnt/stud/work/phahn/repositories/dal-toolbox/perf_dal_new.db'
+mlflow_exp_name='swinv2_baselines'
+
+query_strategies=(alfamix badge bait coreset dropquery margin random typiclust)
+
+datasets=(cifar10 stl10 snacks flowers102 dtd food101 cifar100 tiny_imagenet imagenet)
+acq_sizes=(10 10 20 25 50 100 100 200 1000)
+subset_sizes=(1000 Null Null Null Null 1000 1000 5000 5000)
+
+random_seeds=(1 2 3 4 5 6 7 8 9 10)
+
+index=$SLURM_ARRAY_TASK_ID
+dataset_name=${datasets[$index % 9]}
+acq_size=${acq_sizes[$index % 9]}
+subset_size=${subset_sizes[$index % 9]}
+al_strategy=${query_strategies[$index / 9 % 8]}
+random_seed=${random_seeds[$index / 72]}
+
+if [ $index -eq 0 ]; then
+    python -c "import mlflow; mlflow.set_tracking_uri(r'$mlflow_uri'); mlflow.set_experiment(r'$mlflow_exp_name')"
+fi
+
+date;hostname
+cd /mnt/stud/work/phahn/repositories/dal-toolbox/dal-toolbox/publications/perf_dal
+srun python al.py \
+    dataset_name=$dataset_name \
+    dataset_path=/mnt/stud/work/phahn/datasets \
+    al.strategy=$al_strategy \
+    al.acq_size=$acq_size \
+    al.subset_size=$subset_size \
+    mlflow_uri=$mlflow_uri \
+    al.device=cuda \
+    experiment_name=$mlflow_exp_name \
+    random_seed=$random_seed \
+    backbone=swinv2 \
