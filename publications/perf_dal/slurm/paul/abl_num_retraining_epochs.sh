7f1f50d865c882bd8253280c36314875589f32d5
nhaH-luaP <paul.hahn@uni-kassel.de>
2025-05-09 10:33:49 +0200

    prepared num retraining epochs ablation study

---
+++ publications/perf_dal/slurm/paul/abl_num_retraining_epochs.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+#SBATCH --job-name=optimal_al_baselines
+#SBATCH --partition=main
+#SBATCH --output=/mnt/stud/work/phahn/repositories/dal-toolbox/logs/perf_dal_new_2/%A_%a_%x.log
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=64gb
+#SBATCH --gres=gpu:1
+#SBATCH --array=0-159%4
+source /mnt/stud/work/phahn/venvs/dal-toolbox/bin/activate
+
+mlflow_uri='sqlite:////mnt/stud/work/phahn/repositories/dal-toolbox/perf_dal_new.db'
+mlflow_exp_name='abl_retraining_epochs'
+al_strategy='perf_dal_oracle'
+sel_strats=\[random\]
+var_sss=False
+n_bat=50
+
+num_retraining_epochs=(5 10 25 50 75 100 150 200)
+
+datasets=(cifar10 dtd)
+acq_sizes=(10 50)
+subset_sizes=(1000 Null)
+
+random_seeds=(1 2 3 4 5 6 7 8 9 10)
+
+index=$SLURM_ARRAY_TASK_ID
+dataset_name=${datasets[$index % 2]}
+acq_size=${acq_sizes[$index % 2]}
+subset_size=${subset_sizes[$index % 2]}
+
+n_ret=${num_retraining_epochs[$index / 2 % 8]}
+
+random_seed=${random_seeds[$index / 16]}
+
+if [ $index -eq 0 ]; then
+    python -c "import mlflow; mlflow.set_tracking_uri(r'$mlflow_uri'); mlflow.set_experiment(r'$mlflow_exp_name')"
+fi
+
+date;hostname
+cd /mnt/stud/work/phahn/repositories/dal-toolbox/dal-toolbox/publications/perf_dal
+srun python al.py \
+    dataset_name=$dataset_name \
+    dataset_path=/mnt/stud/work/phahn/datasets \
+    al.strategy=$al_strategy \
+    al.acq_size=$acq_size \
+    al.subset_size=$subset_size \
+    al.optimal.strategies=$sel_strats \
+    al.optimal.vary_strat_subset_size=$var_sss \
+    al.optimal.num_batches=$n_bat \
+    al.optimal.num_retraining_epochs=$n_ret \
+    mlflow_uri=$mlflow_uri \
+    al.device=cuda \
+    experiment_name=$mlflow_exp_name \
+    random_seed=$random_seed \
