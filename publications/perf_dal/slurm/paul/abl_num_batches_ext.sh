7279e477109edaddc00c2ca6ceaf21ce251f8981
nhaH-luaP <paul.hahn@uni-kassel.de>
2025-05-11 09:41:07 +0200

    new exp

---
+++ publications/perf_dal/slurm/paul/abl_num_batches_ext.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+#SBATCH --job-name=optimal_al_baselines
+#SBATCH --partition=main
+#SBATCH --output=/mnt/stud/work/phahn/repositories/dal-toolbox/logs/perf_dal_new_2/%A_%a_%x.log
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=64gb
+#SBATCH --gres=gpu:1
+#SBATCH --array=0-59%4
+source /mnt/stud/work/phahn/venvs/dal-toolbox/bin/activate
+
+mlflow_uri='sqlite:////mnt/stud/work/phahn/repositories/dal-toolbox/perf_dal_new.db'
+mlflow_exp_name='abl_num_batches'
+al_strategy='perf_dal_oracle'
+n_bat=220
+
+selection_strategies=(\[random\] \[random,typiclust,dropquery,bait,typiclass,dropqueryclass,loss,margin,badge,coreset,alfamix\])
+vary_subset_size=(False True)
+
+datasets=(cifar10 dtd)
+acq_sizes=(10 50)
+subset_sizes=(1000 Null)
+
+random_seeds=(1 2 3 4 5 6 7 8 9 10)
+
+index=$SLURM_ARRAY_TASK_ID
+dataset_name=${datasets[$index % 2]}
+acq_size=${acq_sizes[$index % 2]}
+subset_size=${subset_sizes[$index % 2]}
+
+sel_strats=${selection_strategies[$index / 2 % 3]}
+var_sss=${vary_subset_size[$index / 2 % 3]}
+
+random_seed=${random_seeds[$index / 6]}
+
+if [ $index -eq 0 ]; then
+    python -c "import mlflow; mlflow.set_tracking_uri(r'$mlflow_uri'); mlflow.set_experiment(r'$mlflow_exp_name')"
+fi
+
+date;hostname
+cd /mnt/stud/work/phahn/repositories/dal-toolbox/dal-toolbox/publications/perf_dal
+srun python al.py \
+    dataset_name=$dataset_name \
+    dataset_path=/mnt/stud/work/phahn/datasets \
+    al.strategy=$al_strategy \
+    al.acq_size=$acq_size \
+    al.subset_size=$subset_size \
+    al.optimal.strategies=$sel_strats \
+    al.optimal.vary_strat_subset_size=$var_sss \
+    al.optimal.num_batches=$n_bat \
+    mlflow_uri=$mlflow_uri \
+    al.device=cuda \
+    experiment_name=$mlflow_exp_name \
+    random_seed=$random_seed \
