80b815d525b1b195d82240442887e38def734058
nhaH-luaP <paul.hahn@uni-kassel.de>
2025-05-13 00:08:38 +0200

    fixed exp difficulties

--- publications/perf_dal/slurm/paul/abl_num_batches.sh
+++ publications/perf_dal/slurm/paul/abl_num_batches.sh
@@ -6,7 +6,7 @@
 #SBATCH --cpus-per-task=16
 #SBATCH --mem=64gb
 #SBATCH --gres=gpu:1
-#SBATCH --array=0-239%4
+#SBATCH --array=0-159%4
 source /mnt/stud/work/phahn/venvs/dal-toolbox/bin/activate
 
 mlflow_uri='sqlite:////mnt/stud/work/phahn/repositories/dal-toolbox/perf_dal_new.db'
@@ -15,28 +15,29 @@ al_strategy='perf_dal_oracle'
 perf_est=brier
 num_retrain=25
 
+datasets=(cifar10 dtd)
+acq_sizes=(10 50)
+subset_sizes=(1000 Null)
+
 selection_strategies=(\[random\] \[random,typiclust,dropquery,bait,typiclass,dropqueryclass,loss,margin,badge,coreset,alfamix\])
 vary_subset_size=(False True)
 
 num_batches=(11 55 110 220)
 
-datasets=(cifar10 dtd)
-acq_sizes=(10 50)
-subset_sizes=(1000 Null)
-
 random_seeds=(1 2 3 4 5 6 7 8 9 10)
 
 index=$SLURM_ARRAY_TASK_ID
+
 dataset_name=${datasets[$index % 2]}
 acq_size=${acq_sizes[$index % 2]}
 subset_size=${subset_sizes[$index % 2]}
 
-sel_strats=${selection_strategies[$index / 2 % 4]}
-var_sss=${vary_subset_size[$index / 2 % 4]}
+sel_strats=${selection_strategies[$index / 2 % 2]}
+var_sss=${vary_subset_size[$index / 2 % 2]}
 
-n_bat=${num_batches[$index / 8 % 3]}
+n_bat=${num_batches[$index / 4 % 4]}
 
-random_seed=${random_seeds[$index / 24]}
+random_seed=${random_seeds[$index / 16]}
 
 if [ $index -eq 0 ]; then
     python -c "import mlflow; mlflow.set_tracking_uri(r'$mlflow_uri'); mlflow.set_experiment(r'$mlflow_exp_name')"
