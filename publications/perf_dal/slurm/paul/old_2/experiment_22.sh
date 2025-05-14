5493a81b3cde2b0c113f0a9c833873b0f667536f
nhaH-luaP <paul.hahn@uni-kassel.de>
2025-05-14 08:46:05 +0200

    new exps

--- publications/perf_dal/slurm/paul/old_2/experiment_22.sh
+++ publications/perf_dal/slurm/paul/old_2/experiment_22.sh
@@ -10,9 +10,9 @@
 source /mnt/stud/work/phahn/venvs/dal-toolbox/bin/activate
 
 mlflow_uri='sqlite:////mnt/stud/work/phahn/repositories/dal-toolbox/perf_dal_2.db'
-mlflow_exp_name='experiment_17_1'
+mlflow_exp_name='experiment_17_3'
 al_strategy='perf_dal_oracle'
-sel_strats=\[random,typiclust,dropquery,bait,badge,coreset,alfamix\]
+sel_strats=\[random,typiclust,dropquery,bait,coreset\]
 var_sss=True
 n_bat=110
 
