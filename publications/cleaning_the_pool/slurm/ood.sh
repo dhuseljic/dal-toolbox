1c5777cef2ab522767e1c74eaabda296b772e828
Denis Huseljic <dhuseljic@cluster.ies.uni-kassel.de>
2026-01-25 11:52:39 +0100

    added long tailed and ood experiment scripts

--- publications/cleaning_the_pool/slurm/ood.sh
+++ publications/cleaning_the_pool/slurm/ood.sh
@@ -1,33 +1,33 @@
 #!/bin/bash
 #SBATCH --job-name=refine
-#SBATCH --output=/mnt/work/dhuseljic/logs/adaptive_al/long_tailed/%A_%a_%x.log
+#SBATCH --output=/mnt/work/dhuseljic/logs/adaptive_al/ood/%A_%a_%x.log
 #SBATCH --partition=main
 #SBATCH --ntasks=1 
 #SBATCH --cpus-per-task=8
-#SBATCH --mem=32gb
+#SBATCH --mem=16gb
 #SBATCH --gres=gpu:0
 #SBATCH --array=0-9
 ##SBATCH --exclude=gpu-a100-[1-5],gpu-v100-[1-4],gpu-l40s-1
 date;hostname
 source /mnt/home/dhuseljic/envs/dal-toolbox/bin/activate
 export HUGGING_FACE_HUB_TOKEN=$(cat /mnt/home/dhuseljic/.huggingface_token)
+export CUDA_VISIBLE_DEVICES="" # no gpus on l40s pls :)
 ulimit -n 8192
 
-mlflow_uri='sqlite:////mnt/work/dhuseljic/experiments/mlflow/adaptive_al/refine_lt.db'
-mlflow_exp_name='v1'
+mlflow_uri='sqlite:////mnt/work/dhuseljic/experiments/mlflow/adaptive_al/refine_ood.db'
+mlflow_exp_name='v3'
 
 backbones=(dinov2 clip dinov3)
-datasets=('snacks-lt' 'cifar100-lt' 'tiny_imagenet-lt')
+datasets=('blood-mnist' 'derma-mnist')
 strategies=(random select_al tcm tailor autoal refine)
 random_seeds=({1..10})
 
 idx=$SLURM_ARRAY_TASK_ID
 
 backbone=${backbones[0]}
-dataset=${datasets[0]}
-al_strategy=${strategies[1]}
+dataset=${datasets[1]}
+al_strategy=${strategies[5]}
 
-imb_ratio=50
 random_seed=${random_seeds[$idx]}
 
 if [ "$random_seed" -eq 1 ]; then
@@ -37,7 +37,6 @@ cd /mnt/home/dhuseljic/projects/dal-toolbox/publications/cleaning_the_pool/
 srun --mem-bind=local python main.py \
 	dataset=$dataset \
 	dataset.path=/mnt/work/dhuseljic/datasets \
-	dataset.imbalance_ratio=$imb_ratio \
 	model.backbone=$backbone \
 	al.strategy=$al_strategy \
 	al.device=cpu \
