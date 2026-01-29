246f741e20e88cdf538cf240136a104b445c13eb
Denis Huseljic <dhuseljic@cluster.ies.uni-kassel.de>
2026-01-29 11:38:56 +0100

    finetune

--- publications/cleaning_the_pool/slurm/finetune.sh
+++ publications/cleaning_the_pool/slurm/finetune.sh
@@ -6,7 +6,7 @@
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=32gb
 #SBATCH --gres=gpu:1
-#SBATCH --array=0-9
+#SBATCH --array=0-2
 ##SBATCH --exclude=gpu-a100-[1-5],gpu-v100-[1-4],gpu-l40s-1
 date;hostname
 source /mnt/home/dhuseljic/envs/dal-toolbox/bin/activate
@@ -14,16 +14,16 @@ export HUGGING_FACE_HUB_TOKEN=$(cat /mnt/home/dhuseljic/.huggingface_token)
 ulimit -n 8192
 
 mlflow_uri='sqlite:////mnt/work/dhuseljic/experiments/mlflow/adaptive_al/refine_finetune.db'
-mlflow_exp_name='v2'
+mlflow_exp_name='v4'
 
-datasets=('cifar10' 'cifar100')
+datasets=('cifar10' 'cifar100' 'food101')
 strategies=(random select_al tcm tailor autoal refine)
 random_seeds=({1..10})
 
 idx=$SLURM_ARRAY_TASK_ID
 
-dataset=${datasets[0]}
-al_strategy=${strategies[5]}
+dataset=${datasets[1]}
+al_strategy=${strategies[1]}
 random_seed=${random_seeds[$idx]}
 
 if [ "$random_seed" -eq 1 ]; then
@@ -34,6 +34,7 @@ srun --mem-bind=local python main_finetune.py \
 	dataset=$dataset \
 	dataset.path=/mnt/work/dhuseljic/datasets \
 	al.strategy=$al_strategy \
+	model.num_epochs=100 \
 	al.device=cuda \
 	mlflow_uri=$mlflow_uri \
 	experiment_name=$mlflow_exp_name \
